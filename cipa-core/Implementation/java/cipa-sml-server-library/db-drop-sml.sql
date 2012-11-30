d0fb9c15051b00f061f963d04f4e390c249ab9fb
dorazio <dorazio@mycompany.com>
2012-11-30 09:08:49 +0000

    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@29 564210f0-7af0-437e-9e94-ac8d0acde024

--- cipa-core/Implementation/java/cipa-sml-server-library/db-drop-sml.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE sml_allowed_wildcard_schemes DROP FOREIGN KEY FK_sml_allowed_wildcard_schemes_username
-ALTER TABLE sml_recipient_participant_identifier DROP FOREIGN KEY FK_sml_recipient_participant_identifier_smp_id
-ALTER TABLE sml_service_metadata_publisher DROP FOREIGN KEY FK_sml_service_metadata_publisher_username
-DROP TABLE sml_allowed_wildcard_schemes
-DROP TABLE sml_migrate
-DROP TABLE sml_recipient_participant_identifier
-DROP TABLE sml_service_metadata_publisher
-DROP TABLE sml_user
