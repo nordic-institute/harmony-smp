6fb9f558e682b67199c8e1c13f0556df81e5ad96
dorazio <dorazio@mycompany.com>
2012-11-30 15:48:25 +0000

    Initial Commit
    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@74 564210f0-7af0-437e-9e94-ac8d0acde024

---
+++ cipa-addons/Implementation/java/validation-webapp/src/main/java/at/peppol/validation/web/ctrl/DocumentTypeSelect.java
@@ -0,0 +1,59 @@
+/**
+ * Version: MPL 1.1/EUPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Copyright The PEPPOL project (http://www.peppol.eu)
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL
+ * (the "Licence"); You may not use this work except in compliance
+ * with the Licence.
+ * You may obtain a copy of the Licence at:
+ * http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ * If you wish to allow use of your version of this file only
+ * under the terms of the EUPL License and not to allow others to use
+ * your version of this file under the MPL, indicate your decision by
+ * deleting the provisions above and replace them with the notice and
+ * other provisions required by the EUPL License. If you do not delete
+ * the provisions above, a recipient may use your version of this file
+ * under either the MPL or the EUPL License.
+ */
+package at.peppol.validation.web.ctrl;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import at.peppol.validation.rules.EValidationArtefact;
+import at.peppol.validation.rules.EValidationDocumentType;
+
+import com.phloc.html.hc.html.HCSelect;
+
+public final class DocumentTypeSelect extends HCSelect {
+  public DocumentTypeSelect (@Nonnull final String sName, @Nullable final String sSelectedDocType) {
+    super (sName);
+
+    addOption ("", "(Please select an item)");
+    for (final EValidationDocumentType eDocType : EValidationDocumentType.values ()) {
+      // Show only document types for which at least one artefact is present!
+      if (!EValidationArtefact.getAllMatchingArtefacts (null, eDocType, null).isEmpty ())
+        addOption (eDocType.getID (), eDocType.name (), sSelectedDocType);
+    }
+  }
+}
