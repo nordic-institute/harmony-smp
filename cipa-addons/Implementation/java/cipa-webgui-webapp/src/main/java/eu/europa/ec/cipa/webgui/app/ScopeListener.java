0d5ba32aaa9f6de0bf7b6015c7fbc6a46cb76c42
HELGER Philip <philip@helger.com>
2013-10-21 07:53:50 +0000

    Version update
    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@866 564210f0-7af0-437e-9e94-ac8d0acde024

--- cipa-addons/Implementation/java/cipa-webgui-webapp/src/main/java/eu/europa/ec/cipa/webgui/app/ScopeListener.java
+++ cipa-addons/Implementation/java/cipa-webgui-webapp/src/main/java/eu/europa/ec/cipa/webgui/app/ScopeListener.java
@@ -51,10 +51,7 @@ import javax.servlet.http.HttpSessionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import com.phloc.appbasics.app.io.WebFileIO;
-import com.phloc.appbasics.app.io.WebIO;
-import com.phloc.appbasics.app.io.WebIOResourceProviderChain;
 import com.phloc.appbasics.security.AccessManager;
 import com.phloc.appbasics.security.role.RoleManager;
 import com.phloc.appbasics.security.user.UserManager;
@@ -76,7 +73,8 @@ import eu.europa.ec.cipa.webgui.security.CSecurity;
  * 
  * @author philip
  */
-public final class ScopeListener implements ServletContextListener, HttpSessionListener {
+public final class ScopeListener implements ServletContextListener, HttpSessionListener
+{
   public static final String INIT_PARAMETER_TRACE = "trace";
   // like Vaadin:
   public static final String INIT_PARAMETER_DEBUG = "Debug";
@@ -86,7 +84,8 @@ public final class ScopeListener implements ServletContextListener, HttpSessionL
 
   private static final Logger s_aLogger = LoggerFactory.getLogger (ScopeListener.class);
 
-  private static void _initAccessManager () {
+  private static void _initAccessManager ()
+  {
     // Call before accessing AccessManager!
     RoleManager.setCreateDefaults (false);
     UserManager.setCreateDefaults (false);
@@ -133,22 +132,26 @@ public final class ScopeListener implements ServletContextListener, HttpSessionL
       aAM.createPredefinedRole (CSecurity.ROLE_USER_ID, CSecurity.ROLE_USER_NAME);
 
     // User groups
-    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_ADMINISTRATORS_ID)) {
+    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_ADMINISTRATORS_ID))
+    {
       aAM.createPredefinedUserGroup (CSecurity.USERGROUP_ADMINISTRATORS_ID, CSecurity.USERGROUP_ADMINISTRATORS_NAME);
       aAM.assignUserToUserGroup (CSecurity.USERGROUP_ADMINISTRATORS_ID, CSecurity.USER_ADMINISTRATOR_ID);
       aAM.assignRoleToUserGroup (CSecurity.USERGROUP_ADMINISTRATORS_ID, CSecurity.ROLE_ADMINISTRATOR_ID);
     }
-    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_USERS_ID)) {
+    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_USERS_ID))
+    {
       aAM.createPredefinedUserGroup (CSecurity.USERGROUP_USERS_ID, CSecurity.USERGROUP_USERS_NAME);
       aAM.assignRoleToUserGroup (CSecurity.USERGROUP_USERS_ID, CSecurity.ROLE_USER_ID);
     }
-    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_GUESTS_ID)) {
+    if (!aAM.containsUserGroupWithID (CSecurity.USERGROUP_GUESTS_ID))
+    {
       aAM.createPredefinedUserGroup (CSecurity.USERGROUP_GUESTS_ID, CSecurity.USERGROUP_GUESTS_NAME);
     }
   }
 
   @Override
-  public void contextInitialized (final ServletContextEvent sce) {
+  public void contextInitialized (final ServletContextEvent sce)
+  {
     final ServletContext aSC = sce.getServletContext ();
     // set global debug/trace mode
     final boolean bTraceMode = Boolean.parseBoolean (aSC.getInitParameter (INIT_PARAMETER_TRACE));
@@ -161,7 +164,8 @@ public final class ScopeListener implements ServletContextListener, HttpSessionL
     // Set the storage base
     final String sServletContextPath = aSC.getRealPath (".");
     String sBasePath = aSC.getInitParameter (INIT_PARAMETER_STORAGE_BASE);
-    if (StringHelper.hasNoText (sBasePath)) {
+    if (StringHelper.hasNoText (sBasePath))
+    {
       if (GlobalDebug.isDebugMode () && s_aLogger.isInfoEnabled ())
         s_aLogger.info ("No servlet context init-parameter '" +
                         INIT_PARAMETER_STORAGE_BASE +
@@ -171,17 +175,18 @@ public final class ScopeListener implements ServletContextListener, HttpSessionL
     }
     final File aBasePath = new File (sBasePath);
     WebFileIO.initPaths (aBasePath, new File (sServletContextPath), true);
-    WebIO.init (new WebIOResourceProviderChain (aBasePath));
 
     // Init the unique ID provider
     GlobalIDFactory.setPersistentIntIDFactory (new FileIntIDFactory (WebFileIO.getFile ("id.txt")));
 
     // Set the non-multipart file item request web scope
-    MetaWebScopeFactory.setWebScopeFactory (new DefaultWebScopeFactory () {
+    MetaWebScopeFactory.setWebScopeFactory (new DefaultWebScopeFactory ()
+    {
       @Override
       @Nonnull
       public IRequestWebScope createRequestScope (@Nonnull final HttpServletRequest aHttpRequest,
-                                                  @Nonnull final HttpServletResponse aHttpResponse) {
+                                                  @Nonnull final HttpServletResponse aHttpResponse)
+      {
         return new RequestWebScopeNoMultipart (aHttpRequest, aHttpResponse);
       }
     });
@@ -193,19 +198,22 @@ public final class ScopeListener implements ServletContextListener, HttpSessionL
   }
 
   @Override
-  public void contextDestroyed (final ServletContextEvent sce) {
+  public void contextDestroyed (final ServletContextEvent sce)
+  {
     // Destroy global scope
     WebScopeManager.onGlobalEnd ();
   }
 
   @Override
-  public void sessionCreated (final HttpSessionEvent se) {
+  public void sessionCreated (final HttpSessionEvent se)
+  {
     // Create session scope
     WebScopeManager.onSessionBegin (se.getSession ());
   }
 
   @Override
-  public void sessionDestroyed (final HttpSessionEvent se) {
+  public void sessionDestroyed (final HttpSessionEvent se)
+  {
     // Destroy session scope
     WebScopeManager.onSessionEnd (se.getSession ());
   }
