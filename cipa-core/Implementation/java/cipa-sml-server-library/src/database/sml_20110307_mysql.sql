a2bb832676202608ab07a15cd18342c6e05faab8
smetsy <smetsy@mycompany.com>
2014-06-17 10:38:34 +0000

    changed order for execution of statements for the foreign keys.
    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@1124 564210f0-7af0-437e-9e94-ac8d0acde024

--- cipa-core/Implementation/java/cipa-sml-server-library/src/database/sml_20110307_mysql.sql
+++ cipa-core/Implementation/java/cipa-sml-server-library/src/database/sml_20110307_mysql.sql
@@ -44,43 +44,24 @@ USE `sml`;
 -- ------------------------------------------------------
 -- Server version	5.1.43-community
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `migrate`
---
-
 DROP TABLE IF EXISTS `sml_migrate`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sml_migrate` (
   `recipient_participant_identifier_scheme` varchar(200) NOT NULL,
   `recipient_participant_identifier_value` varchar(200) NOT NULL,
   `migration_code` varchar(200) NOT NULL,
   PRIMARY KEY (`recipient_participant_identifier_scheme`,`recipient_participant_identifier_value`,`migration_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `migrate`
---
 
---
--- Table structure for table `allowed_wildcard_schemes`
---
+DROP TABLE IF EXISTS `sml_user`;
+CREATE TABLE `sml_user` (
+  `username` varchar(200) NOT NULL,
+  `password` text,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `sml_allowed_wildcard_schemes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sml_allowed_wildcard_schemes` (
   `scheme` varchar(25) CHARACTER SET latin1 NOT NULL,
   `username` varchar(200) NOT NULL,
@@ -88,19 +69,9 @@ CREATE TABLE `sml_allowed_wildcard_schemes` (
   KEY `new_fk_constraint` (`username`),
   CONSTRAINT `new_fk_constraint` FOREIGN KEY (`username`) REFERENCES `sml_user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `allowed_wildcard_schemes`
---
-
---
--- Table structure for table `service_metadata_publisher`
---
 
 DROP TABLE IF EXISTS `sml_service_metadata_publisher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sml_service_metadata_publisher` (
   `smp_id` varchar(200) NOT NULL,
   `physical_address` text NOT NULL,
@@ -110,37 +81,9 @@ CREATE TABLE `sml_service_metadata_publisher` (
   KEY `FK_service_metadata_publisher_1` (`username`),
   CONSTRAINT `FK_service_metadata_publisher_1` FOREIGN KEY (`username`) REFERENCES `sml_user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `service_metadata_publisher`
---
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `sml_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sml_user` (
-  `username` varchar(200) NOT NULL,
-  `password` text,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
---
--- Table structure for table `recipient_participant_identifier`
---
 
 DROP TABLE IF EXISTS `sml_recipient_participant_identifier`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sml_recipient_participant_identifier` (
   `recipient_participant_identifier_scheme` varchar(200) NOT NULL,
   `recipient_participant_identifier_value` varchar(200) NOT NULL,
@@ -149,19 +92,4 @@ CREATE TABLE `sml_recipient_participant_identifier` (
   KEY `FK_recipient_participant_identifier_1` (`smp_id`),
   CONSTRAINT `FK_recipient_participant_identifier_1` FOREIGN KEY (`smp_id`) REFERENCES `sml_service_metadata_publisher` (`smp_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `recipient_participant_identifier`
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-12-23 19:33:33
