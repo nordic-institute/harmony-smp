3cf71ba5517767787df7abb24377335843e20a82
dorazio <dorazio@mycompany.com>
2014-07-03 09:59:24 +0000

    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@1260 564210f0-7af0-437e-9e94-ac8d0acde024

---
+++ cipa-core/Implementation/java/cipa-dispatcher/src/main/java/eu/europa/ec/cipa/dispatcher/endpoint_interface/IAS4GatewayInterface.java
@@ -0,0 +1,21 @@
+package eu.europa.ec.cipa.dispatcher.endpoint_interface;
+
+import java.security.cert.X509Certificate;
+
+import eu.europa.ec.cipa.dispatcher.exception.DispatcherConfigurationException;
+
+public interface IAS4GatewayInterface {
+
+	/**
+	 * This method creats the Pmode to register a partner in the underlying as4 gateway
+	 * 
+	 * @param partnerGatewayId id of the partner gateway
+	 * @param processId busdox process id, to be mapped to ebms3/AS4 pmode service
+	 * @param documentId busdox process id, to be mapped to ebms3/AS4 pmode action
+	 * @param gatewayURL
+	 * @param cert
+	 * @throws DispatcherConfigurationException
+	 */
+	public void createPartner(String partnerGatewayId,String processId,String documentId,String gatewayURL, X509Certificate cert) throws DispatcherConfigurationException;
+
+}
---
+++ cipa-core/Implementation/java/cipa-dispatcher/src/main/java/eu/europa/ec/cipa/dispatcher/endpoint_interface/domibus/AS4GatewayInterface.java
@@ -0,0 +1,116 @@
+package eu.europa.ec.cipa.dispatcher.endpoint_interface.domibus;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Properties;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import eu.domibus.ebms3.config.PModePool;
+import eu.domibus.ebms3.config.Party;
+import eu.domibus.ebms3.config.Producer;
+import eu.europa.ec.cipa.dispatcher.endpoint_interface.IAS4GatewayInterface;
+import eu.europa.ec.cipa.dispatcher.exception.DispatcherConfigurationException;
+import eu.europa.ec.cipa.dispatcher.util.PropertiesUtil;
+
+public class AS4GatewayInterface implements IAS4GatewayInterface {
+
+	public final static String PMODE_ROLE="GW";
+	public final static String ID_TYPE="urn:oasis:names:tc:ebcore:partyid-type:iso3166-1";
+	private PModePool pmodePool;
+	private File pmodeFile = null;
+	
+	
+	private void initPModePool() throws DispatcherConfigurationException{
+		PropertiesUtil.initializeProperties();
+		Properties properties = PropertiesUtil.getProperties();
+		if (pmodePool == null){
+			String pmodeFilePath = properties.getProperty("as4_pmodeFilePath");
+			pmodeFile = new File(pmodeFilePath);
+			if (!pmodeFile.exists()){
+				try {
+					if (!pmodeFile.createNewFile()){
+						throw new DispatcherConfigurationException("Unable to create pmode file");
+					}
+				} catch (IOException e) {
+					throw new DispatcherConfigurationException("Unable to create pmode file");
+				}
+			}
+			pmodePool = PModePool.load(pmodeFile);
+			if (pmodePool == null){
+				throw new DispatcherConfigurationException("Unable to load PMODEFile");
+			}
+		}
+	}
+	
+	private Producer getProducer(String producerName){
+		List<Producer>  producers = pmodePool.getProducers();
+		for (Producer producer : producers) {
+			if (producer.getName().equalsIgnoreCase(producerName)){
+				return producer;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void createPartner(String partnerGatewayId, String processId,
+			String documentId, String gatewayURL, X509Certificate cert) throws DispatcherConfigurationException {
+		initPModePool();
+		Producer producer = getProducer(partnerGatewayId);
+		Party party = new Party();
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(pmodeFile);
+			if (producer == null){
+				producer= new Producer();
+				producer.setName(partnerGatewayId.concat("-").concat(PMODE_ROLE));
+				producer.setRole(PMODE_ROLE);
+				party.setPartyId(partnerGatewayId);
+				party.setType(ID_TYPE);
+				producer.addParty(party);
+				pmodePool.getProducers().add(producer);
+			}
+            final Serializer serializer = new Persister();
+            serializer.write(pmodePool, fos);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			throw new DispatcherConfigurationException("Unable to load PMODEFile");
+		} catch (IOException e) {
+			throw new DispatcherConfigurationException("Unable to load PMODEFile");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+
+	}
+	
+	public PModePool getPmodePool() {
+		return pmodePool;
+	}
+
+	public void setPmodePool(PModePool pmodePool) {
+		this.pmodePool = pmodePool;
+	}
+
+
+}
