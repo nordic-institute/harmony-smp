220f485a9a7d6fbf71b5b091a13b9ddc92b8f9d6
FERIAL Adrien <adrien.FERIAL@ec.europa.eu>
2015-06-05 13:01:12 +0000

    fix EDELIVERY-398 : Errors when registering records in the DNS
    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@2038 564210f0-7af0-437e-9e94-ac8d0acde024

--- cipa-core/Implementation/java/cipa-sml-server-library/src/main/java/org/xbill/DNS/CustomSIG0.java
+++ cipa-core/Implementation/java/cipa-sml-server-library/src/main/java/org/xbill/DNS/CustomSIG0.java
@@ -1,5 +1,8 @@
 package org.xbill.DNS;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.PrivateKey;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,25 +11,46 @@ import java.util.Date;
  * Created by feriaad on 05/06/2015.
  */
 public class CustomSIG0 {
+
+    static final Logger s_aLogger = LoggerFactory.getLogger(CustomSIG0.class);
+
+    private static final int MAX_RETRY = 5;
+
     public static void signMessage(Message message, KEYRecord key, PrivateKey privkey,
-                SIGRecord previous, int validityMinutesBack) throws DNSSEC.DNSSECException {
+                                   SIGRecord previous, int validityMinutesBack) throws Exception {
+        signMessage(message, key, privkey, previous, validityMinutesBack, 0);
+    }
 
-        int validity = Options.intValue("sig0validity");
-        if (validity < 0)
-            validity = 300; // 5 minutes by default
+    private static void signMessage(Message message, KEYRecord key, PrivateKey privkey,
+                                    SIGRecord previous, int validityMinutesBack, int retry) throws Exception {
+        Exception exception = null;
+        if (retry < MAX_RETRY) {
+            try {
+                int validity = Options.intValue("sig0validity");
 
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MINUTE, -validityMinutesBack);
-        Date minutesBack = cal.getTime();
+                if (validity < 0)
+                    validity = 300; // 5 minutes by default
 
-        long minutesBackBackLong = minutesBack.getTime();
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.MINUTE, -validityMinutesBack);
+                Date minutesBack = cal.getTime();
 
-        Date timeSigned = new Date(minutesBackBackLong);
-        Date timeExpires = new Date(minutesBackBackLong + validity * 1000);
+                long minutesBackBackLong = minutesBack.getTime();
 
-        SIGRecord sig = DNSSEC.signMessage(message, previous, key, privkey,
-                timeSigned, timeExpires);
+                Date timeSigned = new Date(minutesBackBackLong);
+                Date timeExpires = new Date(minutesBackBackLong + validity * 1000);
 
-        message.addRecord(sig, Section.ADDITIONAL);
+                SIGRecord sig = null;
+                sig = DNSSEC.signMessage(message, previous, key, privkey,
+                        timeSigned, timeExpires);
+                message.addRecord(sig, Section.ADDITIONAL);
+            } catch (final Exception exc) {
+                s_aLogger.warn("There was an error when trying to sign the message, trying again. " + retry + " times. Exception was: " + exc.getMessage());
+                signMessage(message, key, privkey, previous, validityMinutesBack, retry + 1);
+                exception = exc;
+            }
+        } else {
+            throw exception;
+        }
     }
 }
