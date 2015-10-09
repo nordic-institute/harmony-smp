a344e09ced335a4ed458515d8c3a649a0fcc9d68
dorazio <dorazio@mycompany.com>
2015-10-09 14:08:47 +0000

    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@2082 564210f0-7af0-437e-9e94-ac8d0acde024

--- domibus/modules/domibus-submission/src/main/java/eu/eCODEX/submission/original/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains a webservice replicating the functionality of {@link eu.domibus.backend}, utilizing the same
- * wsdl. This service is inteded to be a proof-of-concept implementation of the holodeck-submission module and provide
- * a smooth migration path to the new architecture. This webservice can be reached under the context path
- * {@code holodeck/services/Submission_orig_1_1}
- */
-package eu.eCODEX.submission.original;
--- domibus/modules/domibus-submission/src/main/java/eu/eCODEX/submission/transformer/package-info.java
+++
@@ -1,1 +0,0 @@
-package eu.eCODEX.submission.transformer;
