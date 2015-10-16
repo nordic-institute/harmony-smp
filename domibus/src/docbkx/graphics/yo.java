b6a08476d4da30f037c13eb2e32db5ebd8c34353
FERIAL Adrien <adrien.FERIAL@ec.europa.eu>
2015-10-16 07:30:06 +0000

    git-svn-id: https://joinup.ec.europa.eu/svn/cipaedelivery/trunk@2121 564210f0-7af0-437e-9e94-ac8d0acde024

--- domibus/src/docbkx/graphics/yo.java
+++ domibus/src/docbkx/graphics/yo.java
@@ -1,1 +1,13 @@
 ﻿package eu.domibus.submission;
+
+import eu.domibus.submission.transformer.MessageRetrievalTransformer;
+import eu.domibus.submission.transformer.MessageSubmissionTransformer;
+import eu.domibus.submission.transformer.exception.TransformationException;
+import eu.domibus.submission.validation.exception.ValidationException;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+
+import java.util.concurrent.Future;
+
+public class ExampleBackendConnector extends AbstractBackendConnector<MyObjectIn, MyObjectOut>
+                                     implements BackendConnector<MyObjectIn, MyObjectOut>{
