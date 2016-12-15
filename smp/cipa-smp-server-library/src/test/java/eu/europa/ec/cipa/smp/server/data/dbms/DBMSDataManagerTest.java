a3801d59fc49fdcab11b8492d2134dfdebbb4804
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2016-12-15 15:19:23 +0100

    Release 2.5.1-SNAPSHOT

--- smp/cipa-smp-server-library/src/test/java/eu/europa/ec/cipa/smp/server/data/dbms/DBMSDataManagerTest.java
+++ smp/cipa-smp-server-library/src/test/java/eu/europa/ec/cipa/smp/server/data/dbms/DBMSDataManagerTest.java
@@ -58,7 +58,6 @@ import org.busdox.transport.identifiers._1.DocumentIdentifierType;
 import org.busdox.transport.identifiers._1.ParticipantIdentifierType;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
@@ -82,7 +81,7 @@ import eu.europa.ec.cipa.smp.server.hook.DoNothingRegistrationHook;
 /**
  * @author PEPPOL.AT, BRZ, Philip Helger
  */
- @Ignore
+// @Ignore
 // ("Cannot be enabled by default, because it would fail without the correct configuration")
 @DevelopersNote ("You need to adjust your local config.properties file to run this test")
 public class DBMSDataManagerTest {
