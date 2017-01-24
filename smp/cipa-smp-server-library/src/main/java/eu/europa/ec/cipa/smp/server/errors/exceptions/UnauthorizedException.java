bc75e50b63652a38e630d4ac266f36b6b5c2e5ff
Flavio Santos <flavio.santos@ext.ec.europa.eu>
2017-01-24 13:41:58 +0100

    EDELIVERY-1709 - implementing tests

--- smp/cipa-smp-server-library/src/main/java/eu/europa/ec/cipa/smp/server/errors/exceptions/UnauthorizedException.java
+++ smp/cipa-smp-server-library/src/main/java/eu/europa/ec/cipa/smp/server/errors/exceptions/UnauthorizedException.java
@@ -1,18 +1,14 @@
 /**
  * Version: MPL 1.1/EUPL 1.1
- * <p>
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at:
  * http://www.mozilla.org/MPL/
- * <p>
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * <p>
  * The Original Code is Copyright The PEPPOL project (http://www.peppol.eu)
- * <p>
  * Alternatively, the contents of this file may be used under the
  * terms of the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL
@@ -20,13 +16,11 @@
  * with the Licence.
  * You may obtain a copy of the Licence at:
  * http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
- * <p>
  * If you wish to allow use of your version of this file only
  * under the terms of the EUPL License and not to allow others to use
  * your version of this file under the MPL, indicate your decision by
