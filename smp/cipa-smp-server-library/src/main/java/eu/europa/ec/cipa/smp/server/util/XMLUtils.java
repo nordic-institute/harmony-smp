46ab6233272ac67d6d905348c346ba8bfb042ead
Tiago Miguel <tiago.miguel@ext.ec.europa.eu>
2017-02-06 16:23:28 +0100

    Correction on XMLUtils

--- smp/cipa-smp-server-library/src/main/java/eu/europa/ec/cipa/smp/server/util/XMLUtils.java
+++ smp/cipa-smp-server-library/src/main/java/eu/europa/ec/cipa/smp/server/util/XMLUtils.java
@@ -30,7 +30,7 @@ public class XMLUtils {
 
     private static final Pattern pat = Pattern.compile("-?[0-9a-fA-F]+");
 
-    public static final QName EXT_TYPE_QNAME = new QName("http://busdox.org/serviceMetadata/publishing/1.0/", "ExtensionType");
+    public static final QName EXT_TYPE_QNAME = new QName("http://docs.oasis-open.org/bdxr/bdx-smp/v1.0/", "ExtensionType");
 
     /**
      * Converts an XML object to a string.
@@ -132,7 +132,6 @@ public class XMLUtils {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             XMLOutputFactory xof = XMLOutputFactory.newFactory();
             XMLStreamWriter xsw = xof.createXMLStreamWriter(baos);
-            xsw = new MyXMLStreamWriter(xsw);
             jaxbMarshaller.marshal(aJaxbElement, xsw);
             xsw.close();
             return baos.toString();
@@ -142,176 +141,6 @@ public class XMLUtils {
             LOGGER.error(xSex.getMessage());
         }
         return null;
-    }
-
-
-    public static class MyXMLStreamWriter implements XMLStreamWriter {
-
-        private XMLStreamWriter xsw;
-        private MyNamespaceContext nc = new MyNamespaceContext();
-
-        public MyXMLStreamWriter(XMLStreamWriter xsw) throws XMLStreamException {
-            this.xsw = xsw;
-            xsw.setNamespaceContext(nc);
-        }
-
-        public void close() throws XMLStreamException {
-            xsw.close();
-        }
-
-        public void flush() throws XMLStreamException {
-            xsw.flush();
-        }
-
-        public javax.xml.namespace.NamespaceContext getNamespaceContext() {
-            return xsw.getNamespaceContext();
-        }
-
-        public String getPrefix(String arg0) throws XMLStreamException {
-            return xsw.getPrefix(arg0);
-        }
-
-        public Object getProperty(String arg0) throws IllegalArgumentException {
-            return xsw.getProperty(arg0);
-        }
-
-        public void setDefaultNamespace(String arg0) throws XMLStreamException {
-            xsw.setDefaultNamespace(arg0);
-        }
-
-        public void setNamespaceContext(NamespaceContext arg0) throws XMLStreamException {
-        }
-
-        public void setPrefix(String arg0, String arg1) throws XMLStreamException {
-            xsw.setPrefix(arg0, arg1);
-        }
-
-        public void writeAttribute(String arg0, String arg1) throws XMLStreamException {
-            xsw.writeAttribute(arg0, arg1);
-        }
-
-        public void writeAttribute(String arg0, String arg1, String arg2) throws XMLStreamException {
-            xsw.writeAttribute(arg0, arg1, arg2);
-        }
-
-        public void writeAttribute(String arg0, String arg1, String arg2, String arg3) throws XMLStreamException {
-            xsw.writeAttribute(arg0, arg1, arg2, arg3);
-        }
-
-        public void writeCData(String arg0) throws XMLStreamException {
-            xsw.writeCData(arg0);
-        }
-
-        public void writeCharacters(String arg0) throws XMLStreamException {
-            xsw.writeCharacters(arg0);
-        }
-
-        public void writeCharacters(char[] arg0, int arg1, int arg2) throws XMLStreamException {
-            xsw.writeCharacters(arg0, arg1, arg2);
-        }
-
-        public void writeComment(String arg0) throws XMLStreamException {
-            xsw.writeComment(arg0);
-        }
-
-        public void writeDTD(String arg0) throws XMLStreamException {
-            xsw.writeDTD(arg0);
-        }
-
-        public void writeDefaultNamespace(String arg0) throws XMLStreamException {
-            xsw.writeDefaultNamespace(arg0);
-        }
-
-        public void writeEmptyElement(String arg0) throws XMLStreamException {
-            xsw.writeEmptyElement(arg0);
-        }
-
-        public void writeEmptyElement(String arg0, String arg1) throws XMLStreamException {
-            xsw.writeEmptyElement(arg0, arg1);
-        }
-
-        public void writeEmptyElement(String arg0, String arg1, String arg2) throws XMLStreamException {
-            xsw.writeEmptyElement(arg0, arg1, arg2);
-        }
-
-        public void writeEndDocument() throws XMLStreamException {
-            xsw.writeEndDocument();
-        }
-
-        public void writeEndElement() throws XMLStreamException {
-            xsw.writeEndElement();
-        }
-
-        public void writeEntityRef(String arg0) throws XMLStreamException {
-            xsw.writeEntityRef(arg0);
-        }
-
-        public void writeNamespace(String arg0, String arg1) throws XMLStreamException {
-        }
-
-        public void writeProcessingInstruction(String arg0) throws XMLStreamException {
-            xsw.writeProcessingInstruction(arg0);
-        }
-
-        public void writeProcessingInstruction(String arg0, String arg1) throws XMLStreamException {
-            xsw.writeProcessingInstruction(arg0, arg1);
-        }
-
-        public void writeStartDocument() throws XMLStreamException {
-            xsw.writeStartDocument();
-        }
-
-        public void writeStartDocument(String arg0) throws XMLStreamException {
-            xsw.writeStartDocument(arg0);
-        }
-
-        public void writeStartDocument(String arg0, String arg1) throws XMLStreamException {
-            xsw.writeStartDocument(arg0, arg1);
-        }
-
-        public void writeStartElement(String arg0) throws XMLStreamException {
-            xsw.writeStartElement(arg0);
-        }
-
-        public void writeStartElement(String arg0, String arg1) throws XMLStreamException {
-            xsw.writeStartElement(arg0, arg1);
-        }
-
-        public void writeStartElement(String arg0, String arg1, String arg2) throws XMLStreamException {
-            xsw.writeStartElement("", arg1, arg2);
-            if (null != arg2 || arg2.length() > 0) {
-                String currentDefaultNS = nc.getNamespaceURI("");
-                if (!arg2.equals(currentDefaultNS)) {
-                    writeDefaultNamespace(arg2);
-                    nc.setDefaultNS(arg2);
-                }
-            }
-        }
-    }
-
-    private static class MyNamespaceContext implements NamespaceContext {
-
-        private String defaultNS = "";
-
-        public void setDefaultNS(String ns) {
-            defaultNS = ns;
-        }
-
-        public String getNamespaceURI(String arg0) {
-            if ("".equals(arg0)) {
-                return defaultNS;
-            }
-            return null;
-        }
-
-        public String getPrefix(String arg0) {
-            return "";
-        }
-
-        public Iterator getPrefixes(String arg0) {
-            return null;
-        }
-
     }
 
 }
