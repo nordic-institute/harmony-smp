c97dca67660bce7d3547ade2b79809fb0466f5fc
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2017-02-09 18:14:17 +0100

    EDELIVERY-1726 - Removed legacy and garbage modules :)

--- as4/Domibus-MSH/src/main/webapp/WEB-INF/views/home.jsp
+++
@@ -1,9 +0,0 @@
-<%@page session="true" %>
-<html>
-<head>
-    <jsp:include page="header.jsp"/>
-</head>
-<body>
-<h1>${title}</h1>
-</body>
-</html>
--- as4/domibus-app/domibus-base/src/main/java/eu/europa/ec/digit/domibus/base/config/DataSourceConfiguration.java
+++
@@ -1,8 +0,0 @@
-package eu.europa.ec.digit.domibus.base.config;
-
-import javax.sql.DataSource;
-
-public interface DataSourceConfiguration {
-
-	public DataSource dataSource();
-}
