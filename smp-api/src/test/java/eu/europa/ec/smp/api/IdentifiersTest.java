facb78f75e6b4fb0ac5986b2a5e9ebee4cfbcd38
Tiago Miguel <tiago.miguel@ext.ec.europa.eu>
2017-02-13 13:13:52 +0100

    Included new test cases based on eHealth needs.

--- smp-api/src/test/java/eu/europa/ec/smp/api/IdentifiersTest.java
+++ smp-api/src/test/java/eu/europa/ec/smp/api/IdentifiersTest.java
@@ -26,7 +26,30 @@ public class IdentifiersTest {
                 {"a:b::c", "a:b", "c"},
                 {"ehealth-actorid-qns::urn:poland:ncpb", "ehealth-actorid-qns", "urn:poland:ncpb"},
                 {"ehealth-resid-qns::urn::epsos##services:extended:epsos::51", "ehealth-resid-qns", "urn::epsos##services:extended:epsos::51"},
-                {"iso6523-actorid-upis::0002:gutek", "iso6523-actorid-upis", "0002:gutek"}
+                {"iso6523-actorid-upis::0002:gutek", "iso6523-actorid-upis", "0002:gutek"},
+                {"ehealth-actorid-qns::urn:ehealth:hr:ncpb-idp","ehealth-actorid-qns","urn:ehealth:hr:ncpb-idp"},
+                {"scheme::urn:ehealth:pt:ncpb-idp","scheme","urn:ehealth:pt:ncpb-idp"},
+                {"otherscheme::urn:ehealth:be:ncpb-idp","otherscheme","urn:ehealth:be:ncpb-idp"},
+                {"ehealth-resid-qns::urn:ehealth:IdentityService::XCPD::CrossGatewayPatientDiscovery##ITI-55","ehealth-resid-qns","urn:ehealth:IdentityService::XCPD::CrossGatewayPatientDiscovery##ITI-55"},
+                {"ehealth-resid-qns::urn:XCPD::CrossGatewayPatientDiscovery","ehealth-resid-qns","urn:XCPD::CrossGatewayPatientDiscovery"},
+                {"ehealth-resid-qns::urn:ehealth:PatientService::XCA::CrossGatewayQuery##ITI-38","ehealth-resid-qns","urn:ehealth:PatientService::XCA::CrossGatewayQuery##ITI-38"},
+                {"ehealth-resid-qns::urn:XCA::CrossGatewayQuery","ehealth-resid-qns","urn:XCA::CrossGatewayQuery"},
+                {"ehealth-resid-qns::urn:ehealth:OrderService::XCA::CrossGatewayQuery##ITI-38","ehealth-resid-qns","urn:ehealth:OrderService::XCA::CrossGatewayQuery##ITI-38"},
+                {"ehealth-resid-qns::urn:ehealth:DispensationService:Initialize::XDR::ProvideandRegisterDocumentSet-b##ITI-41","ehealth-resid-qns","urn:ehealth:DispensationService:Initialize::XDR::ProvideandRegisterDocumentSet-b##ITI-41"},
+                {"ehealth-resid-qns::urn:XDR::ProvideandRegisterDocumentSet-b","ehealth-resid-qns","urn:XDR::ProvideandRegisterDocumentSet-b"},
+                {"ehealth-resid-qns::urn:ehealth:DispensationService:Discard::XDR::ProvideandRegisterDocumentSet-b##ITI-41","ehealth-resid-qns","urn:ehealth:DispensationService:Discard::XDR::ProvideandRegisterDocumentSet-b##ITI-41"},
+                {"ehealth-resid-qns::urn:ehealth:ConsentService:Put::XDR::BPPCProvideandRegisterDocumentSet-b##ITI-41","ehealth-resid-qns","urn:ehealth:ConsentService:Put::XDR::BPPCProvideandRegisterDocumentSet-b##ITI-41"},
+                {"ehealth-resid-qns::urn:XDR::BPPCProvideandRegisterDocumentSet-b","ehealth-resid-qns","urn:XDR::BPPCProvideandRegisterDocumentSet-b"},
+                {"ehealth-resid-qns::urn:ehealth:ConsentService:Discard::XDR::BPPCProvideandRegisterDocumentSet-b##ITI-41","ehealth-resid-qns","urn:ehealth:ConsentService:Discard::XDR::BPPCProvideandRegisterDocumentSet-b##ITI-41"},
+                {"ehealth-resid-qns::urn:ehealth:IdP::identityProvider::HPAuthentication##epsos-91","ehealth-resid-qns","urn:ehealth:IdP::identityProvider::HPAuthentication##epsos-91"},
+                {"ehealth-resid-qns::urn:identityProvider::HPAuthentication","ehealth-resid-qns","urn:identityProvider::HPAuthentication"},
+                {"ehealth-resid-qns::urn:ehealth:IdP::XUA::ProvideX-UserAssertion##ITI-40","ehealth-resid-qns","urn:ehealth:IdP::XUA::ProvideX-UserAssertion##ITI-40"},
+                {"ehealth-resid-qns::urn:XUA::ProvideX-UserAssertion","ehealth-resid-qns","urn:XUA::ProvideX-UserAssertion"},
+                {"ehealth-resid-qns::urn:ehealth:VPN::VPNGatewayServer##epsos-105","ehealth-resid-qns","urn:ehealth:VPN::VPNGatewayServer##epsos-105"},
+                {"ehealth-resid-qns::urn:ehealth:ncp::vpngateway","ehealth-resid-qns","urn:ehealth:ncp::vpngateway"},
+                {"ehealth-resid-qns::urn:ehealth:VPN::VPNGatewayClient##epsos-106","ehealth-resid-qns","urn:ehealth:VPN::VPNGatewayClient##epsos-106"},
+                {"ehealth-resid-qns::urn:ehealth:ISM::InternationalSearchMask##epsos-107","ehealth-resid-qns","urn:ehealth:ISM::InternationalSearchMask##epsos-107"},
+                {"ehealth-resid-qns::urn:ehealth:ncp::pt:ism","ehealth-resid-qns","urn:ehealth:ncp::pt:ism"}
         };
     }
 
