ace6467d8ef4131c940867c1e8ceff3be61c3eba
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2017-02-22 19:43:09 +0100

    EDELIVERY-1744 Added JUnit test for password hashing tool

---
+++ smp-server-library/src/test/java/eu/europa/ec/cipa/smp/server/security/BCryptPasswordHashTest.java
@@ -0,0 +1,66 @@
+package eu.europa.ec.cipa.smp.server.security;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gutowpa on 22/02/2017.
+ */
+public class BCryptPasswordHashTest {
+
+    private static final String PASSWORD = "this_is_sample_password";
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void generatedHashIsValidTest() {
+        //when
+        String hash = BCryptPasswordHash.hashPassword(PASSWORD);
+
+        //then
+        assertTrue(BCrypt.checkpw(PASSWORD, hash));
+    }
+
+    @Test
+    public void generatedHashIsAlwaysSaltedTest() {
+        //when
+        String hash1 = BCryptPasswordHash.hashPassword(PASSWORD);
+        String hash2 = BCryptPasswordHash.hashPassword(PASSWORD);
+
+        //then
+        assertNotEquals(hash1, hash2);
+    }
+
+    @Test
+    public void mainMethodSupportsMultiplePasswordsAndPrintsThemToStandardOutputTest() {
+        //given
+        String[] passwords = new String[]{PASSWORD + 1, PASSWORD + 2, PASSWORD + 3};
+
+        //when
+        BCryptPasswordHash.main(passwords);
+
+        //then
+        String[] hashes = outContent.toString().replaceAll("\r", "").split("\n");
+        assertEquals(passwords.length, hashes.length);
+        for (int i = 0; i < passwords.length; i++) {
+            assertTrue(BCrypt.checkpw(passwords[i], hashes[i]));
+        }
+    }
+}
