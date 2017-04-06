317ea8aafa6bac546fab34b1547b0c82314a8b8e
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2017-04-06 13:23:49 +0200

    EDELIVERY-1977 Implemented BlueCoatAuthenticationFilter and PreAuthenticatedCertificatePrincipal - with JUnit tests

---
+++ smp-webapp/src/main/java/eu/europa/ec/cipa/smp/server/security/PreAuthenticatedCertificatePrincipal.java
@@ -0,0 +1,13 @@
+package eu.europa.ec.cipa.smp.server.security;
+
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+/**
+ * Created by gutowpa on 04/04/2017.
+ */
+public class PreAuthenticatedCertAuthenticationToken extends PreAuthenticatedAuthenticationToken {
+
+    public PreAuthenticatedCertAuthenticationToken(String subjectDN, String issuerDN){
+        super
+    }
+}
---
+++ smp-webapp/src/test/java/eu/europa/ec/cipa/smp/server/security/PreAuthenticatedCertificatePrincipalTest.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.cipa.smp.server.security;
+
+/**
+ * Created by gutowpa on 05/04/2017.
+ */
+public class PreAuthenticatedCertificatePrincipalTest {
+}
