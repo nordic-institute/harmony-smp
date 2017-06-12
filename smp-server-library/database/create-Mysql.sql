dd4bb9a5efc8a57200e80d3e246faa044a120f10
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2017-06-12 19:13:35 +0200

    EDELIVERY-1742 Updated MySQL script, added migration scripts, refactored.

--- smp-server-library/database/create-Mysql.sql
+++ smp-server-library/database/create-Mysql.sql
@@ -13,124 +13,97 @@
 -- See the Licence for the specific language governing permissions and limitations under the Licence.
 
 
--- Not dumping tablespaces as no INFORMATION_SCHEMA.FILES table on this server
---
-
-
-DROP TABLE IF EXISTS smp_service_group;
 CREATE TABLE smp_service_group (
-  businessIdentifier varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  businessIdentifierScheme varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  extension BLOB,
-  PRIMARY KEY  (businessIdentifier,businessIdentifierScheme)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  businessIdentifier       VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  businessIdentifierScheme VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  extension                TEXT             NULL DEFAULT NULL,
+  PRIMARY KEY (businessIdentifier, businessIdentifierScheme)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS smp_service_metadata;
+
 CREATE TABLE smp_service_metadata (
-  documentIdentifier varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  documentIdentifierScheme varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  businessIdentifier varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  businessIdentifierScheme varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  extension TEXT,
-  xmlcontent TEXT,
-  PRIMARY KEY  (documentIdentifier,documentIdentifierScheme,businessIdentifier,businessIdentifierScheme),
-  KEY FK_service_metadata_1 (businessIdentifier,businessIdentifierScheme),
-  CONSTRAINT FK_service_metadata_1 FOREIGN KEY (businessIdentifier, businessIdentifierScheme) REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  documentIdentifier       VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  documentIdentifierScheme VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  businessIdentifier       VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  businessIdentifierScheme VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  extension                TEXT             NULL DEFAULT NULL,
+  xmlcontent               TEXT,
+  PRIMARY KEY (documentIdentifier, documentIdentifierScheme, businessIdentifier, businessIdentifierScheme),
+  KEY FK_service_metadata_service_group (businessIdentifier, businessIdentifierScheme),
+  CONSTRAINT FK_service_metadata_service_group FOREIGN KEY (businessIdentifier, businessIdentifierScheme) REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS smp_process;
-CREATE TABLE smp_process (
-  processIdentifier varchar(256) NOT NULL,
-  processIdentifierType varchar(256) NOT NULL,
-  extension BLOB,
-  businessIdentifier varchar(256) NOT NULL,
-  businessIdentifierScheme varchar(256) NOT NULL,
-  documentIdentifier varchar(256) NOT NULL,
-  documentIdentifierScheme varchar(256) NOT NULL,
-  PRIMARY KEY  (processIdentifier,processIdentifierType,businessIdentifier,businessIdentifierScheme,documentIdentifier,documentIdentifierScheme),
-  KEY FK_process_1 (documentIdentifier,documentIdentifierScheme,businessIdentifier,businessIdentifierScheme),
-  CONSTRAINT FK_process_1 FOREIGN KEY (documentIdentifier, documentIdentifierScheme, businessIdentifier, businessIdentifierScheme) REFERENCES smp_service_metadata (documentIdentifier, documentIdentifierScheme, businessIdentifier, businessIdentifierScheme) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS smp_user;
 CREATE TABLE smp_user (
-  username varchar(256) NOT NULL,
-  password varchar(256),
-  isadmin tinyint(1) DEFAULT 0 NOT NULL,
-  PRIMARY KEY  (username)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  username VARCHAR(256)         NOT NULL,
+  password VARCHAR(256),
+  isadmin  TINYINT(1) DEFAULT 0 NOT NULL,
+  PRIMARY KEY (username)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS smp_service_metadata_red;
-CREATE TABLE smp_service_metadata_red (
-  businessIdentifier varchar(256) NOT NULL,
-  businessIdentifierScheme varchar(256) NOT NULL,
-  documentIdentifier varchar(256) NOT NULL,
-  documentIdentifierScheme varchar(256) NOT NULL,
-  redirectionUrl varchar(256) NOT NULL,
-  extension TEXT,
-  certificateUID varchar(256) NOT NULL,
-  PRIMARY KEY  (businessIdentifier,businessIdentifierScheme,documentIdentifier,documentIdentifierScheme)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS smp_endpoint;
-CREATE TABLE smp_endpoint (
-  transportProfile varchar(256) NOT NULL,
-  endpointReference varchar(256) NOT NULL,
-  extension BLOB,
-  processIdentifier varchar(256) NOT NULL,
-  processIdentifierType varchar(256) NOT NULL,
-  businessIdentifier varchar(256) NOT NULL,
-  businessIdentifierScheme varchar(256) NOT NULL,
-  documentIdentifier varchar(256) NOT NULL,
-  documentIdentifierScheme varchar(256) NOT NULL default '',
-  requireBusinessLevelSignature bit(1) NOT NULL,
-  minimumAuthenticationLevel varchar(256) default NULL,
-  serviceActivationDate datetime default NULL,
-  serviceExpirationDate datetime NOT NULL,
-  certificate TEXT NOT NULL,
-  serviceDescription BLOB NOT NULL,
-  technicalContactUrl varchar(256) NOT NULL,
-  technicalInformationUrl varchar(256) default NULL,
-  PRIMARY KEY  (businessIdentifierScheme,businessIdentifier,processIdentifierType,processIdentifier,documentIdentifier,documentIdentifierScheme,endpointReference,transportProfile),
-  KEY FK_endpoint_1 (processIdentifier,processIdentifierType,businessIdentifier,businessIdentifierScheme,documentIdentifier,documentIdentifierScheme),
-  CONSTRAINT FK_endpoint_1 FOREIGN KEY (processIdentifier, processIdentifierType, businessIdentifier, businessIdentifierScheme, documentIdentifier, documentIdentifierScheme) REFERENCES smp_process (processIdentifier, processIdentifierType, businessIdentifier, businessIdentifierScheme, documentIdentifier, documentIdentifierScheme) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS smp_ownership;
 CREATE TABLE smp_ownership (
-  username varchar(256) NOT NULL,
-  businessIdentifier varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  businessIdentifierScheme varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  KEY FK_ownership_1 (businessIdentifier,businessIdentifierScheme),
-  KEY FK_ownership_2 (username),
-  CONSTRAINT FK_ownership_2 FOREIGN KEY (username) REFERENCES smp_user (username) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT FK_ownership_1 FOREIGN KEY (businessIdentifier, businessIdentifierScheme) REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE smp_service_group
-CHANGE COLUMN extension extension TEXT NULL DEFAULT NULL ;
-
-ALTER TABLE smp_service_metadata
-CHANGE COLUMN extension extension TEXT NULL DEFAULT NULL ;
+  username                 VARCHAR(256)     NOT NULL,
+  businessIdentifier       VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  businessIdentifierScheme VARCHAR(256)
+                           CHARACTER SET utf8
+                           COLLATE utf8_bin NOT NULL,
+  KEY FK_ownership_service_group (businessIdentifier, businessIdentifierScheme),
+  KEY FK_ownership_user (username),
+  CONSTRAINT FK_ownership_service_group FOREIGN KEY (businessIdentifier, businessIdentifierScheme) REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT FK_ownership_user FOREIGN KEY (username) REFERENCES smp_user (username)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 DROP TRIGGER IF EXISTS smp_user_check_is_admin_value_before_insert;
 DROP TRIGGER IF EXISTS smp_user_check_is_admin_value_before_update;
-delimiter //
-CREATE TRIGGER smp_user_check_is_admin_value_before_insert BEFORE INSERT ON smp_user
+DELIMITER //
+CREATE TRIGGER smp_user_check_is_admin_value_before_insert
+BEFORE INSERT ON smp_user
 FOR EACH ROW
-BEGIN
-	IF NEW.ISADMIN <> 0 AND NEW.ISADMIN <> 1 THEN
-		SIGNAL SQLSTATE '99999'
-		SET MESSAGE_TEXT = '0 or 1 are the only allowed values for ISADMIN column';
-	END IF;
-END //
-CREATE TRIGGER smp_user_check_is_admin_value_before_update BEFORE UPDATE ON smp_user
+  BEGIN
+    IF NEW.ISADMIN <> 0 AND NEW.ISADMIN <> 1
+    THEN
+      SIGNAL SQLSTATE '99999'
+      SET MESSAGE_TEXT = '0 or 1 are the only allowed values for ISADMIN column';
+    END IF;
+  END //
+CREATE TRIGGER smp_user_check_is_admin_value_before_update
+BEFORE UPDATE ON smp_user
 FOR EACH ROW
-BEGIN
-	IF NEW.ISADMIN <> 0 AND NEW.ISADMIN <> 1 THEN
-		SIGNAL SQLSTATE '99999'
-		SET MESSAGE_TEXT = '0 or 1 are the only allowed values for ISADMIN column';
-	END IF;
-END //
-delimiter ;
+  BEGIN
+    IF NEW.ISADMIN <> 0 AND NEW.ISADMIN <> 1
+    THEN
+      SIGNAL SQLSTATE '99999'
+      SET MESSAGE_TEXT = '0 or 1 are the only allowed values for ISADMIN column';
+    END IF;
+  END //
+DELIMITER ;
--- smp-server-library/database/create-Oracle.sql
+++ smp-server-library/database/create-Oracle.sql
@@ -13,48 +13,54 @@
 -- See the Licence for the specific language governing permissions and limitations under the Licence.
 
 CREATE TABLE smp_ownership (
-username VARCHAR(256) NOT NULL, 
-businessIdentifier VARCHAR(50) NOT NULL, 
-businessIdentifierScheme VARCHAR(100) NOT NULL,
-PRIMARY KEY (username, businessIdentifier, businessIdentifierScheme));
+  username                 VARCHAR(256) NOT NULL,
+  businessIdentifier       VARCHAR(50)  NOT NULL,
+  businessIdentifierScheme VARCHAR(100) NOT NULL,
+  PRIMARY KEY (username, businessIdentifier, businessIdentifierScheme)
+);
 
 CREATE TABLE smp_service_group (
-extension CLOB, 
-businessIdentifier VARCHAR(50) NOT NULL, 
-businessIdentifierScheme VARCHAR(100) NOT NULL,
-PRIMARY KEY (businessIdentifier, businessIdentifierScheme));
+  extension                CLOB,
+  businessIdentifier       VARCHAR(50)  NOT NULL,
+  businessIdentifierScheme VARCHAR(100) NOT NULL,
+  PRIMARY KEY (businessIdentifier, businessIdentifierScheme)
+);
 
 CREATE TABLE smp_service_metadata (
-extension CLOB, 
-documentIdentifierScheme VARCHAR(100) NOT NULL,
-businessIdentifier VARCHAR(50) NOT NULL, 
-businessIdentifierScheme VARCHAR(100) NOT NULL,
-documentIdentifier VARCHAR(500) NOT NULL,
-xmlcontent CLOB,
-PRIMARY KEY (
-documentIdentifierScheme, 
-businessIdentifier, 
-businessIdentifierScheme, 
-documentIdentifier));
+  extension                CLOB,
+  documentIdentifierScheme VARCHAR(100) NOT NULL,
+  businessIdentifier       VARCHAR(50)  NOT NULL,
+  businessIdentifierScheme VARCHAR(100) NOT NULL,
+  documentIdentifier       VARCHAR(500) NOT NULL,
+  xmlcontent               CLOB,
+  PRIMARY KEY (
+    documentIdentifierScheme,
+    businessIdentifier,
+    businessIdentifierScheme,
+    documentIdentifier)
+);
 
 CREATE TABLE smp_user (
-username VARCHAR(256), 
-password VARCHAR(256),
-isadmin NUMBER(1) DEFAULT 0 NOT NULL,
-PRIMARY KEY (username),
-CONSTRAINT check_is_admin_value CHECK (isadmin = 0 OR isadmin = 1));
+  username VARCHAR(256),
+  password VARCHAR(256),
+  isadmin  NUMBER(1) DEFAULT 0 NOT NULL,
+  PRIMARY KEY (username),
+  CONSTRAINT check_is_admin_value CHECK (isadmin = 0 OR isadmin = 1)
+);
 
 
+ALTER TABLE smp_ownership
+  ADD CONSTRAINT
+  FK_smp_ownership_username FOREIGN KEY (username) REFERENCES smp_user (username);
 
-ALTER TABLE smp_ownership ADD CONSTRAINT 
-FK_smp_ownership_username FOREIGN KEY (username) REFERENCES smp_user (username);
-
-ALTER TABLE smp_ownership ADD CONSTRAINT 
-FK_smp_ownership_businessId FOREIGN KEY(
-businessIdentifier, businessIdentifierScheme) 
+ALTER TABLE smp_ownership
+  ADD CONSTRAINT
+  FK_smp_ownership_businessId FOREIGN KEY (
+    businessIdentifier, businessIdentifierScheme)
 REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme);
 
-ALTER TABLE smp_service_metadata ADD CONSTRAINT 
-FK_smp_service_meta_busId FOREIGN KEY (
-businessIdentifier, businessIdentifierScheme) 
+ALTER TABLE smp_service_metadata
+  ADD CONSTRAINT
+  FK_smp_service_meta_busId FOREIGN KEY (
+    businessIdentifier, businessIdentifierScheme)
 REFERENCES smp_service_group (businessIdentifier, businessIdentifierScheme);
