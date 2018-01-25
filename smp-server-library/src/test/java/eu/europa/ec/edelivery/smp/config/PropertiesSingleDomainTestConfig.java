456023efec02b214e75e213958fa7d6395e37e5c
Pawel GUTOWSKI <Pawel.GUTOWSKI@ext.ec.europa.eu>
2018-01-25 16:08:23 +0100

    EDELIVERY-1893 Added JUnit tests for multiple certificates and domains support

--- smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/config/PropertiesSingleDomainTestConfig.java
+++ smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/config/PropertiesSingleDomainTestConfig.java
@@ -14,27 +14,25 @@
 package eu.europa.ec.edelivery.smp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-import java.util.Map;
-import java.util.Properties;
+import static eu.europa.ec.edelivery.smp.testutil.LocalPropertiesTestUtil.buildLocalProperties;
 
 /**
- * Created by gutowpa on 24/01/2018.
+ * Created by gutowpa on 11/01/2018.
  */
-public abstract class AbstractPropertiesTestConfig {
-
-    protected Map<String, String> overridenProperties;
+@Configuration
+@PropertySource("classpath:config.properties")
+public class PropertiesSingleDomainTestConfig {
 
     @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        PropertySourcesPlaceholderConfigurer propertiesConfig = new PropertySourcesPlaceholderConfigurer();
-
-        Properties localProps = new Properties();
-        overridenProperties.forEach((key, value) -> localProps.setProperty(key, value));
-        propertiesConfig.setProperties(localProps);
-        propertiesConfig.setLocalOverride(true);
-
-        return propertiesConfig;
+    public PropertySourcesPlaceholderConfigurer setLocalProperties() {
+        String signingKeystorePath = Thread.currentThread().getContextClassLoader().getResource("service_integration_signatures_single_domain.jks").getFile();
+        return buildLocalProperties(new String[][]{
+                {"xmldsig.keystore.classpath", signingKeystorePath},
+                {"xmldsig.keystore.password", "test123"}
+        });
     }
 }
