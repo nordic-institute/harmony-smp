b53373a3600cea02f769443402db4085aeb58c51
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2018-10-09 16:57:46 +0200

    EDELIVERY-3687 SMP UI Add/Edit user
    Replace the roles present in the security service with the ones presen
    in our roles enumeration.

--- smp-angular/src/app/security/role.model.ts
+++ smp-angular/src/app/security/role.model.ts
@@ -1,5 +1,14 @@
 export enum Role {
-  SMP_ADMINISTRATOR,
-  SERVICE_GROUP_ADMINISTRATOR,
+  /**
+   * The system administrator (a.k.a. the "super admin") role
+   */
   SYSTEM_ADMINISTRATOR,
+  /**
+   * The SMP Administrator role. It is assimilable to the {@link SERVICE_GROUP_ADMINISTRATOR} role for now.
+    */
+  SMP_ADMINISTRATOR,
+  /**
+   * The ServiceGroup administrator role
+   */
+  SERVICE_GROUP_ADMINISTRATOR,
 }
