da14fcc2988567193d97795321d8ed1269005dc5
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2018-10-09 16:57:46 +0200

    EDELIVERY-3687 SMP UI Add/Edit user
    Replace the roles present in the security service with the ones presen
    in our roles enumeration.

--- smp-angular/src/app/security/is-authorized.directive.ts
+++ smp-angular/src/app/security/is-authorized.directive.ts
@@ -1,25 +1,27 @@
 import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {SecurityService} from './security.service';
+import {Role} from "./role.model";
 
 @Directive({
     selector:'[isAuthorized]'
 })
 export class IsAuthorized implements OnInit {
-    @Input('isAuthorized') role:string;
+    @Input('isAuthorized') role: Role;
+
     constructor(private _elementRef:ElementRef, private securityService:SecurityService) {
     }
 
-    ngOnInit():void {
-      if(this.role && this.role.trim() !== '') {
+    ngOnInit() {
+      if(this.role) {
         this.securityService.isAuthorized([this.role]).subscribe((isAuthorized:boolean) => {
           if(!isAuthorized) {
             let el : HTMLElement = this._elementRef.nativeElement;
             el.parentNode.removeChild(el);
           }
         },
-          (error:any) => {
-            console.log("Error in IsAuthorized directive [" + error + "]");
-          });
+        (error:any) => {
+          console.log("Error in IsAuthorized directive [" + error + "]");
+        });
       }
     }
 }
