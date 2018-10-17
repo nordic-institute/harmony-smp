5f2808b29d350b9878d25e96b82a99ef7a25524d
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-10-17 10:27:31 +0200

    Delete extension dialog is not needed anymore

--- smp-angular/src/app/service-group-edit/service-group-edit-controller.ts
+++ smp-angular/src/app/service-group-edit/service-group-edit-controller.ts
@@ -1,7 +1,6 @@
 import {SearchTableController} from '../common/search-table/search-table-controller';
 import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material';
 import {ServiceGroupDetailsDialogComponent} from './service-group-details-dialog/service-group-details-dialog.component';
-import {ServiceGroupExtensionDialogComponent} from './service-group-extension-dialog/service-group-extension-dialog.component';
 import {ServiceGroupEditRo} from './service-group-edit-ro.model';
 import {SearchTableEntityStatus} from '../common/search-table/search-table-entity-status.model';
 import {ServiceMetadataEditRo} from "./service-metadata-edit-ro.model";
@@ -18,14 +17,6 @@ export class ServiceGroupEditController implements SearchTableController {
       //Todo:
     });
 
-  }
-
-  public showExtension(row: any) {
-    let dialogRef: MatDialogRef<ServiceGroupExtensionDialogComponent> = this.dialog.open(ServiceGroupExtensionDialogComponent);
-    dialogRef.componentInstance.servicegroup = row;
-    dialogRef.afterClosed().subscribe(result => {
-      //Todo:
-    });
   }
 
   public showMetadataList(row: any) {
