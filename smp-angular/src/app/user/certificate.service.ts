141a49d4e6a66211545b910180a80d3b01d422f0
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2018-10-18 03:54:26 +0200

    EDELIVERY-3687 SMP UI Add/Edit user
    Rename validUntil to validTo as present in the back-end.
    Add support for fingerprints when reading the uploaded certificate.
    Add Jackson 2 support for JSR310 dates and configure a Jackson 2
    HttpMessageConverter.

--- smp-angular/src/app/user/certificate.service.ts
+++ smp-angular/src/app/user/certificate.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {CertificateRo} from './certificate-ro.model';
 import {HttpClient} from '@angular/common/http';
+import {SmpConstants} from "../smp.constants";
 
 @Injectable()
 export class CertificateService {
@@ -9,6 +10,6 @@ export class CertificateService {
   constructor(private http: HttpClient) {}
 
   uploadCertificate$(payload): Observable<CertificateRo> {
-    return this.http.post<CertificateRo>('rest/user/certdata', payload);
+    return this.http.post<CertificateRo>(SmpConstants.REST_CERTIFICATE, payload);
   }
 }
