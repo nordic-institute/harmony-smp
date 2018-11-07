01aa112e3f051d89a1a1de09061642a8f5c833e9
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2018-11-07 15:34:15 +0100

    EDELIVERY-4054 Existing password is being accepted in change password
    Allow normal authenticated users to upload certificates as part of
    editing their details.

--- smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/auth/SMPAuthorizationService.java
+++ smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/auth/SMPAuthorizationService.java
@@ -5,11 +5,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import static eu.europa.ec.edelivery.smp.auth.SMPAuthority.S_AUTHORITY_TOKEN_SYSTEM_ADMIN;
+
 /**
  * @author Sebastian-Ion TINCU
  */
 @Service("smpAuthorizationService")
 public class SMPAuthorizationService {
+
+    public boolean isSystemAdministrator() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication instanceof SMPAuthenticationToken
+                && authentication.getAuthorities().stream().anyMatch(grantedAuthority -> S_AUTHORITY_TOKEN_SYSTEM_ADMIN.equals(grantedAuthority.getAuthority()));
+    }
 
     public boolean isCurrentlyLoggedIn(Long userId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
