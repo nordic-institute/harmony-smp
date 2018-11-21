3a3132b6076ade5b6c548d946926174a7e12e9b0
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-11-21 11:34:33 +0100

    - fix x-forwarded parameters

--- smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/controllers/ServiceMetadataPathBuilder.java
+++ smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/controllers/ServiceMetadataPathBuilder.java
@@ -13,6 +13,9 @@
 
 package eu.europa.ec.edelivery.smp.controllers;
 
+import eu.europa.ec.edelivery.smp.logging.SMPLogger;
+import eu.europa.ec.edelivery.smp.logging.SMPLoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.oasis_open.docs.bdxr.ns.smp._2016._05.DocumentIdentifier;
 import org.oasis_open.docs.bdxr.ns.smp._2016._05.ParticipantIdentifierType;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +25,7 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.HttpServletRequest;
@@ -35,8 +39,31 @@ import static eu.europa.ec.smp.api.Identifiers.asUrlEncodedString;
 @Component
 public class ServiceMetadataPathBuilder {
 
+    private static final SMPLogger LOG = SMPLoggerFactory.getLogger( ServiceMetadataPathBuilder.class);
+
+
     @Value("${contextPath.output:true}")
     private boolean keepContext;
+
+
+    enum ForwardedHeaderNameEnum {
+        HOST("X-Forwarded-Host"),
+        PORT("X-Forwarded-Port"),
+        PROTO("X-Forwarded-Proto"),
+        PREFIX("X-Forwarded-Prefix"),
+        SSL("X-Forwarded-Ssl"),
+        FOR("X-Forwarded-For");
+
+        String headerName;
+
+        ForwardedHeaderNameEnum(String headerName) {
+            this.headerName = headerName;
+        }
+
+        public String getHeaderName() {
+            return headerName;
+        }
+    }
 
     public URI getCurrentUri() {
         return ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUri();
@@ -44,14 +71,28 @@ public class ServiceMetadataPathBuilder {
 
     public String buildSelfUrl(ParticipantIdentifierType participantId, DocumentIdentifier docId) {
 
-        String path = ServletUriComponentsBuilder.fromCurrentRequestUri()
-                .replacePath(getUrlContext())
+        HttpServletRequest req = getCurrentRequest();
+        ForwardedHeaders fh = new ForwardedHeaders(req);
+        LOG.info("Generate response uri for forwareded headers: " + fh.toString());
+
+        UriComponentsBuilder uriBuilder = ServletUriComponentsBuilder.fromCurrentRequestUri();
+        uriBuilder = uriBuilder.replacePath(getUrlContext());
+        if (fh.getHost()!=null) {
+            uriBuilder = uriBuilder.host(fh.getHost());
+            if (!StringUtils.isBlank(fh.getPort())) {
+                uriBuilder = uriBuilder.port(fh.getPort());
+            }
+            uriBuilder = uriBuilder.scheme(fh.getProto());
+        }
+
+        String path = uriBuilder
                 .path("/{participantId}/services/{docId}")
                 .buildAndExpand(asUrlEncodedString(participantId), asUrlEncodedString(docId))
                 .toUriString();
 
         return path;
     }
+
 
     private String getUrlContext() {
         if (keepContext) {
@@ -68,5 +109,61 @@ public class ServiceMetadataPathBuilder {
         HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
         Assert.state(servletRequest != null, "Could not find current HttpServletRequest");
         return servletRequest;
+    }
+
+    private static class ForwardedHeaders {
+        final String host;
+        final String port;
+        final String proto;
+        final String forClientHost;
+        final String ssl;
+
+
+        public ForwardedHeaders(HttpServletRequest request) {
+            if (request != null) {
+                host = request.getHeader(ForwardedHeaderNameEnum.HOST.getHeaderName());
+                port = request.getHeader(ForwardedHeaderNameEnum.PORT.getHeaderName());;
+                proto = request.getHeader(ForwardedHeaderNameEnum.PROTO.getHeaderName());;
+                forClientHost = request.getHeader(ForwardedHeaderNameEnum.FOR.getHeaderName());;
+                ssl = request.getHeader(ForwardedHeaderNameEnum.SSL.getHeaderName());;
+            } else {
+                host = null;
+                port = null;
+                proto = null;
+                forClientHost = null;
+                ssl = null;
+            }
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public String getPort() {
+            return port;
+        }
+
+        public String getProto() {
+            return proto;
+        }
+
+        public String getForClientHost() {
+            return forClientHost;
+        }
+
+        public String getSsl() {
+            return ssl;
+        }
+
+        @Override
+        public String toString() {
+            return "ForwardedHeaders{" +
+                    "host='" + host + '\'' +
+                    ", port='" + port + '\'' +
+                    ", proto='" + proto + '\'' +
+                    ", forClientHost='" + forClientHost + '\'' +
+                    ", ssl='" + ssl + '\'' +
+                    '}';
+        }
     }
 }
