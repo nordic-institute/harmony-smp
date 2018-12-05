fa92d75dcfd266328d7096799e39659ce011f7cb
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-12-05 12:22:19 +0100

    Update update for sonar B warnings

--- smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/sml/SMLErrorMessages.java
+++ smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/sml/SMLErrorMessages.java
@@ -2,9 +2,9 @@ package eu.europa.ec.edelivery.smp.sml;
 
 public class SMLErrorMessages {
 
-    public static String ERR_PARTICIPANT_ALREADY_EXISTS ="[ERR-106] The participant identifier '%s' does already exist for the scheme %s";
-    public static String ERR_DOMAIN_ALREADY_EXISTS="[ERR-106] The SMP '%s' already exists";
-    public static String ERR_DOMAIN_NOT_EXISTS="[ERR-100] The SMP '%s' doesn't exist";
+    public static final String ERR_PARTICIPANT_ALREADY_EXISTS ="[ERR-106] The participant identifier '%s' does already exist for the scheme %s";
+    public static final String ERR_DOMAIN_ALREADY_EXISTS="[ERR-106] The SMP '%s' already exists";
+    public static final String ERR_DOMAIN_NOT_EXISTS="[ERR-100] The SMP '%s' doesn't exist";
 
 
 }
