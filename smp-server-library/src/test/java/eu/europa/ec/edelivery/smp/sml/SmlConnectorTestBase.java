509a941bbbacd215462eb178666d1fd8b34d4e55
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-12-06 12:39:00 +0100

    unit test for SML integration - fix for sonar

--- smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorTestBase.java
+++ smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorTestBase.java
@@ -6,8 +6,6 @@ import eu.europa.ec.edelivery.smp.config.SmlIntegrationConfiguration;
 import eu.europa.ec.edelivery.smp.data.model.DBDomain;
 import eu.europa.ec.edelivery.smp.services.SecurityUtilsServices;
 import eu.europa.ec.edelivery.smp.services.ui.UIKeystoreService;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.oasis_open.docs.bdxr.ns.smp._2016._05.ParticipantIdentifierType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { SmlConnector.class,SmlIntegrationConfiguration.class,
+@ContextConfiguration(classes = {SmlConnector.class, SmlIntegrationConfiguration.class,
         SecurityUtilsServices.class, UIKeystoreService.class,
         ConversionTestConfig.class, PropertiesSingleDomainTestConfig.class})
 @Configuration
@@ -27,29 +25,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class SmlConnectorTestBase {
     protected static final ParticipantIdentifierType PARTICIPANT_ID = new ParticipantIdentifierType("sample:value", "sample:scheme");
     protected static final DBDomain DEFAULT_DOMAIN;
+
     static {
         DEFAULT_DOMAIN = new DBDomain();
         DEFAULT_DOMAIN.setDomainCode("default_domain_id");
         DEFAULT_DOMAIN.setSmlSmpId("SAMPLE-SMP-ID");
     }
 
-    protected static final String  ERROR_UNEXPECTED_MESSAGE ="[ERR-106] Something unexpected happend";
-    protected static final String  ERROR_SMP_NOT_EXISTS ="[ERR-100] The SMP '"+DEFAULT_DOMAIN.getSmlSmpId()+"' doesn't exist";
-    protected static final String  ERROR_SMP_ALREADY_EXISTS ="[ERR-106] The SMP '"+DEFAULT_DOMAIN.getSmlSmpId()+"' already exists";
-    protected static final String  ERROR_PI_ALREADY_EXISTS = "[ERR-106] The participant identifier 'sample:value' does already exist for the scheme sample:scheme";
-    protected static final String  ERROR_PI_NO_EXISTS = "[ERR-100] The participant identifier 'sample:value' doesn't exist for the scheme sample:scheme";
+    protected static final String ERROR_UNEXPECTED_MESSAGE = "[ERR-106] Something unexpected happend";
+    protected static final String ERROR_SMP_NOT_EXISTS = "[ERR-100] The SMP '" + DEFAULT_DOMAIN.getSmlSmpId() + "' doesn't exist";
+    protected static final String ERROR_SMP_ALREADY_EXISTS = "[ERR-106] The SMP '" + DEFAULT_DOMAIN.getSmlSmpId() + "' already exists";
+    protected static final String ERROR_PI_ALREADY_EXISTS = "[ERR-106] The participant identifier 'sample:value' does already exist for the scheme sample:scheme";
+    protected static final String ERROR_PI_NO_EXISTS = "[ERR-100] The participant identifier 'sample:value' doesn't exist for the scheme sample:scheme";
 
 
-
-    @Rule
-    public ExpectedException expectedExeption = ExpectedException.none();
     @Autowired
     protected SmlConnector smlConnector;
     @Autowired
     SmlIntegrationConfiguration mockSml;
 
-    @Autowired
-    public void setup(){
-        mockSml.reset();
-    }
 }
--- smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorTestDomain.java
+++ smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorTestDomain.java
@@ -17,24 +17,13 @@ import eu.europa.ec.bdmsl.ws.soap.BadRequestFault;
 import eu.europa.ec.bdmsl.ws.soap.InternalErrorFault;
 import eu.europa.ec.bdmsl.ws.soap.NotFoundFault;
 import eu.europa.ec.bdmsl.ws.soap.UnauthorizedFault;
-import eu.europa.ec.edelivery.smp.config.ConversionTestConfig;
-import eu.europa.ec.edelivery.smp.config.PropertiesSingleDomainTestConfig;
-import eu.europa.ec.edelivery.smp.config.SmlIntegrationConfiguration;
-import eu.europa.ec.edelivery.smp.data.model.DBDomain;
 import eu.europa.ec.edelivery.smp.exceptions.SMPRuntimeException;
-import eu.europa.ec.edelivery.smp.services.SecurityUtilsServices;
-import eu.europa.ec.edelivery.smp.services.ui.UIKeystoreService;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.oasis_open.docs.bdxr.ns.smp._2016._05.ParticipantIdentifierType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -46,35 +35,13 @@ import static org.mockito.Mockito.verify;
  * since 4.1.
  */
 @RunWith(SpringRunner.class)
-public class SmlConnectorTestDomain  extends SmlConnectorTestBase{
-
-   // private static List<IManageParticipantIdentifierWS> smlClientMocks = new ArrayList<>();
-    private static final ParticipantIdentifierType PARTICIPANT_ID = new ParticipantIdentifierType("sample:value", "sample:scheme");
-    private static final DBDomain DEFAULT_DOMAIN;
-
-    static {
-        DEFAULT_DOMAIN = new DBDomain();
-        DEFAULT_DOMAIN.setDomainCode("default_domain_id");
-        DEFAULT_DOMAIN.setSmlSmpId("SAMPLE-SMP-ID");
-    }
-
-    private static final String  ERROR_UNEXPECTED_MESSAGE ="[ERR-106] Something unexpected happend";
-    private static final String  ERROR_SMP_NOT_EXISTS ="[ERR-100] The SMP '"+DEFAULT_DOMAIN.getSmlSmpId()+"' doesn't exist";
-    private static final String  ERROR_SMP_ALREADY_EXISTS ="[ERR-106] The SMP '"+DEFAULT_DOMAIN.getSmlSmpId()+"' already exists";
-    private static final String  ERROR_PI_ALREADY_EXISTS = "[ERR-106] The participant identifier 'sample:value' does already exist for the scheme sample:scheme";
-    private static final String  ERROR_PI_NO_EXISTS = "[ERR-100] The participant identifier 'sample:value' doesn't exist for the scheme sample:scheme";
+public class SmlConnectorTestDomain extends SmlConnectorTestBase {
 
     @Rule
     public ExpectedException expectedExeption = ExpectedException.none();
 
-    @Autowired
-    SmlIntegrationConfiguration mockSml;
-
-    @Autowired
-    private SmlConnector smlConnector;
-
-    @Autowired
-    public void setup(){
+    @Before
+    public void setup() {
         mockSml.reset();
     }
 
@@ -105,13 +72,13 @@ public class SmlConnectorTestDomain  extends SmlConnectorTestBase{
     }
 
     @Test
-    public void testRegisterDomainInDnsUnknownException(){
+    public void testRegisterDomainInDnsUnknownException() {
         //when
         String message = "something unexpected";
         Exception ex = new Exception(message);
         mockSml.setThrowException(ex);
         expectedExeption.expectMessage(message);
-        expectedExeption.expect(SMPRuntimeException .class);
+        expectedExeption.expect(SMPRuntimeException.class);
 
         smlConnector.registerDomain(DEFAULT_DOMAIN);
     }
@@ -155,47 +122,47 @@ public class SmlConnectorTestDomain  extends SmlConnectorTestBase{
     }
 
     @Test
-    public void testUnregisterDomainFromDnsThrowUnknownBadRequestFault()  {
+    public void testUnregisterDomainFromDnsThrowUnknownBadRequestFault() {
         //when
         BadRequestFault ex = new BadRequestFault(ERROR_UNEXPECTED_MESSAGE);
         mockSml.setThrowException(ex);
         expectedExeption.expectMessage(ERROR_UNEXPECTED_MESSAGE);
-        expectedExeption.expect(SMPRuntimeException .class);
+        expectedExeption.expect(SMPRuntimeException.class);
 
         smlConnector.unregisterDomain(DEFAULT_DOMAIN);
     }
 
-   @Test
-    public void testUnregisterDomainFromDnsThrowUnknownException()  {
+    @Test
+    public void testUnregisterDomainFromDnsThrowUnknownException() {
         //when
         String message = "something unexpected";
         Exception ex = new Exception(message);
         mockSml.setThrowException(ex);
         expectedExeption.expectMessage(message);
-        expectedExeption.expect(SMPRuntimeException .class);
+        expectedExeption.expect(SMPRuntimeException.class);
 
-       smlConnector.unregisterDomain(DEFAULT_DOMAIN);
+        smlConnector.unregisterDomain(DEFAULT_DOMAIN);
     }
 
     @Test
-    public void testUnregisterDomainFromDnsNotExists()  {
+    public void testUnregisterDomainFromDnsNotExists() {
         //when
         BadRequestFault ex = new BadRequestFault(ERROR_SMP_NOT_EXISTS);
         mockSml.setThrowException(ex);
-        boolean  suc = smlConnector.unregisterDomain(DEFAULT_DOMAIN);
+        boolean suc = smlConnector.unregisterDomain(DEFAULT_DOMAIN);
 
         assertTrue(suc);
     }
 
     @Test
-    public void testIsOkMessageForDomainNull(){
+    public void testIsOkMessageForDomainNull() {
         boolean suc = smlConnector.isOkMessage(DEFAULT_DOMAIN, null);
 
         assertFalse(suc);
     }
 
     @Test
-    public void testIsOkMessageForDomainFalse(){
+    public void testIsOkMessageForDomainFalse() {
 
         boolean suc = smlConnector.isOkMessage(DEFAULT_DOMAIN, ERROR_UNEXPECTED_MESSAGE);
 
