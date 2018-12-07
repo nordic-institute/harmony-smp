48311a9abd5cc673a97f64efc846542d3d9ff3c3
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-12-07 08:23:37 +0100

    Change SML test classes to end with test

--- smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorParticipantTest.java
+++ smp-server-library/src/test/java/eu/europa/ec/edelivery/smp/sml/SmlConnectorParticipantTest.java
@@ -49,7 +49,7 @@ import static org.mockito.Mockito.verify;
         ConversionTestConfig.class, PropertiesSingleDomainTestConfig.class})
 @TestPropertySource(properties = {
         "bdmsl.integration.enabled=true"})
-public class SmlConnectorParticipantIntegrationTest  {
+public class SmlConnectorParticipantTest  {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
