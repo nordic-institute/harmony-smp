5273e8e0ca0f6708c379cb64c9d702bfe1d5eeb3
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2019-01-11 14:57:46 +0100

    Add database column description

--- smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/data/model/DBServiceMetadataXml.java
+++ smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/data/model/DBServiceMetadataXml.java
@@ -1,5 +1,6 @@
 package eu.europa.ec.edelivery.smp.data.model;
 
+import eu.europa.ec.edelivery.smp.data.dao.utils.ColumnDescription;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
@@ -16,16 +17,19 @@ import java.util.Objects;
 @Entity
 @Audited
 @Table(name = "SMP_SERVICE_METADATA_XML")
+@org.hibernate.annotations.Table(appliesTo = "SMP_SERVICE_METADATA_XML", comment = "Service group metadata xml blob")
 @NamedQueries({
         @NamedQuery(name = "DBServiceMetadataXml.deleteById", query = "DELETE FROM DBServiceMetadataXml d WHERE d.id = :id"),
 })
 public class DBServiceMetadataXml extends BaseEntity {
 
     @Id
+    @ColumnDescription(comment = "Shared primary key with master table SMP_SERVICE_METADATA")
     private Long id;
 
     @Lob
     @Column(name = "XML_CONTENT")
+    @ColumnDescription(comment = "XML service metadata ")
     byte[] xmlContent;
 
     @OneToOne
