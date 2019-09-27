bf6cf6b0ebb21cf04cf0f52f9777fba92b937d7c
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2019-09-27 20:34:37 +0200

    Add oracle docker files

---
+++ smp-docker/compose/weblogic-oracle/runCompose.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+WORKING_DIR="$(dirname $0)"
+SMP_INIT_DATABASE="../../../smp-webapp/src/main/smp-setup/database-scripts/oracle10g.ddl"
+SMP_INIT_DATABASE_DATA="../../../smp-webapp/src/main/smp-setup/database-scripts/oracle10g-data.sql"
+# soap ui data
+PREFIX="smp-wls-orcl"
+
+
+
+echo "Working Directory: ${WORKING_DIR}"
+cd "$WORKING_DIR"
+# clear volume and containers - to run  restart from strach 
+
+
+# READ argumnets 
+while getopts i: option
+do
+  case "${option}"
+  in
+    i) SMP_INIT_DATABASE_DATA=${OPTARG};;
+  esac
+done
+
+
+# create  database init script from 
+echo "CONNECT smp/test@//localhost:1521/xe;" > ./properties/db-scripts/02_oracle10g.sql
+cat  "${SMP_INIT_DATABASE}" >> ./properties/db-scripts/02_oracle10g.sql
+
+
+
+# copy init database data for  SMP    
+if [ ! -f "${SMP_INIT_DATABASE_DATA}" ]
+  then
+  echo "SMP sql init data '${SMP_INIT_DATABASE_DATA} not found!!"
+  exit 1;
+else
+  # copy artefact to docker build folder
+  echo "CONNECT smp/test@//localhost:1521/xe;" > ./properties/db-scripts/03_oracle10g-data.sql
+  cat  "${SMP_INIT_DATABASE_DATA}" >> ./properties/db-scripts/03_oracle10g-data.sql
+fi
+
+
+
+
+function clearOldContainers {
+  echo "Clear containers and volumes"
+  docker-compose -p "${PREFIX}" rm -s -f -v
+  docker volume rm "${PREFIX}_shared-status-folder"
+}
+
+
+#
+# Always delete shared-status-folder else weblogic will start to quick!
+# because statuses are sync over shared-status-folder folders and it could contain status from previous run.
+
+clearOldContainers
+# start 
+docker-compose -p ${PREFIX} up -d --force-recreate
+
+# wait until service is up
+for i in `seq 100`; do timeout 1  bash -c ' curl --head --silent --fail http://localhost:7901/smp/'; if [ $? -eq 0  ] ; then break;fi; echo "$i. Wait for weblogic to start!";  sleep 5;  done;
+
---
+++ smp-docker/compose/weblogic-oracle/stopClearCompose.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WORKING_DIR="$(dirname $0)"
+echo "Working Directory: ${WORKING_DIR}"
+cd "$WORKING_DIR"
+
+PREFIX="smp-wls-orcl"
+
+# clear volume and containers - to run  restart from strach 
+
+
+
+function clearOldContainers {
+  echo "Clear containers and volumes"
+  docker-compose -p "${PREFIX}" rm -s -f -v
+  docker volume rm "${PREFIX}_shared-status-folder"
+
+}
+
+
+# stop and clear  
+clearOldContainers
+
