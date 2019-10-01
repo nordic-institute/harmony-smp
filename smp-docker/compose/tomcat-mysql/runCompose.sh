380e8ea341cd118d2259e6fce227eb77f62f395d
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2019-10-01 08:18:09 +0200

    update docker images for pushing to dockerhub

--- smp-docker/compose/tomcat-mysql/runCompose.sh
+++ smp-docker/compose/tomcat-mysql/runCompose.sh
@@ -5,22 +5,35 @@ SML_INIT_DATABASE="../../../smp-webapp/src/main/smp-setup/database-scripts/mysql
 SML_INIT_DATABASE_DATA="../../../smp-webapp/src/main/smp-setup/database-scripts/mysql5innodb-data.sql"
 # soap ui data
 PREFIX="smp-tomcat-mysql"
+SMP_VERSION=
 
-
-
-echo "Working Directory: ${WORKING_DIR}"
-cd "$WORKING_DIR"
 # clear volume and containers - to run  restart from strach 
 
 
 # READ argumnets 
-while getopts i: option
+while getopts i:v: option
 do
   case "${option}"
   in
     i) SML_INIT_DATABASE_DATA=${OPTARG};;
+    v) SMP_VERSION=${OPTARG};;
   esac
 done
+
+
+if [  -z "${SMP_VERSION}" ]
+then
+  # get version from POM file 
+  
+  SMP_VERSION="$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)"
+  
+fi
+
+echo "SMP version: $SMP_VERSION"
+echo "Working Directory: ${WORKING_DIR}"
+cd "$WORKING_DIR"
+
+
 
 # check if property folder exists if not create it
 if  [ ! -d "./properties/db-scripts/" ]
@@ -36,7 +49,8 @@ cp   "${SML_INIT_DATABASE_DATA}" ./properties/db-scripts/mysql5innodb-data.sql
 
 function clearOldContainers {
   echo "Clear containers and volumes"
-  docker-compose -p "${PREFIX}" rm -s -f -v
+  docker-compose -p "${PREFIX}" rm -s -f -v 
+  echo "Clear containers and volumes"
 }
 
 
@@ -44,9 +58,12 @@ function clearOldContainers {
 # Always delete shared-status-folder else weblogic will start to quick!
 # because statuses are sync over shared-status-folder folders and it could contain status from previous run.
 
+export SMP_VERSION="${SMP_VERSION}"
+echo "Clear old containser"
 clearOldContainers
-# start 
-docker-compose -p ${PREFIX} up -d --force-recreate
+# start " 
+echo "Start compose"
+docker-compose -p ${PREFIX} up -d --force-recreate 
 
 # wait until service is up
 for i in `seq 100`; do timeout 1  bash -c ' curl --head --silent --fail http://localhost:8981/smp/'; if [ $? -eq 0  ] ; then break;fi; echo "$i. Wait for tomcat to start!";  sleep 5;  done;
