19e27740accb61e7b7a67c8ea5960919995e9f1f
monhaso <sonali.mohanty@ext.ec.europa.eu>
2022-06-08 12:28:44 +0200

    1)Necessary locators are changed and some locator got added. 2)Some testcases got modified in user page since the functionliyty of user page got changed.

--- smp-ui-tests/src/main/java/utils/enums/SMPMessages.java
+++ smp-ui-tests/src/main/java/utils/enums/SMPMessages.java
@@ -69,13 +69,7 @@ public class SMPMessages {
 
 
 	public static final String USERNAME_VALIDATION_MESSAGE = "Username can only contain alphanumeric characters (letters A-Z, numbers 0-9) and must have from 4 to 32 characters!";
-	public static final String PASS_POLICY_MESSAGE = "Password should follow all of these rules:\n" +
-			"- Minimum length: 8 characters\n" +
-			"- Maximum length: 32 characters\n" +
-			"- At least one letter in lowercase\n" +
-			"- At least one letter in uppercase\n" +
-			"- At least one digit\n" +
-			"- At least one special character";
+	public static final String PASS_POLICY_MESSAGE = "Minimum length: 16 characters;Maximum length: 32 characters;At least one letter in lowercase;At least one letter in uppercase;At least one digit;At least one special character";
 	public static final String PASS_NO_MATCH_MESSAGE = "Passwords do not match";
 	public static final String PASS_NO_EMPTY_MESSAGE = "You should type a password";
 
