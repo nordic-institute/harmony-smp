843d92dea3d798efe39cecbae949d60d91f8bb5f
Catalin Comanici <Catalin.COMANICI@ext.ec.europa.eu>
2022-08-03 18:58:55 +0200

    minor fixes and reformating

--- smp-ui-tests/src/main/java/pages/users/UserPopup.java
+++ smp-ui-tests/src/main/java/pages/users/UserPopup.java
@@ -1,7 +1,6 @@
 package pages.users;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,234 +9,227 @@ import pages.components.ConfirmationDialog;
 import pages.components.GenericSelect;
 import pages.components.baseComponents.PageComponent;
 
-import static org.openqa.selenium.remote.DriverCommand.CLICK_ELEMENT;
-
 public class UserPopup extends PageComponent {
-	public UserPopup(WebDriver driver) {
-		super(driver);
-		PageFactory.initElements(driver, this);
-		rolesSelect = new GenericSelect(driver, rolesSelectContainer);
-	}
+    public GenericSelect rolesSelect;
+    @FindBy(id = "userDetailsToggle_id")
+    WebElement userDetailsToggle;
+
+    @FindBy(css = "#active_id > label > div > div")
+    WebElement activeToggle;
+
+    @FindBy(id = "username_id")
+    WebElement userNameInput;
+
+    @FindBy(id = "emailAddress_id")
+    WebElement emailInput;
+
+    @FindBy(id = "np_id")
+    WebElement newPasswordInput;
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @FindBy(id = "cnp_id")
+    WebElement confirmationInput;
+
+    @FindBy(css = "input#cp_id")
+    WebElement adminPassInput;
+
+    @FindBy(css = "mat-form-field.username> div > div.mat-form-field-flex > div > div")
+    WebElement usernameValidationError;
+
+    @FindBy(css = "smp-password-change-dialog .password-panel mat-form-field:nth-child(2) .mat-form-field-subscript-wrapper mat-error")
+    WebElement passValidationError;
+
+    @FindBy(css = "mat-form-field.password-confirmation > div > div.mat-form-field-flex > div > div")
+    WebElement passConfirmationValidationError;
+
+    @FindBy(css = ".ng-trigger.ng-trigger-transitionMessages.ng-star-inserted > mat-error")
+    WebElement passMatchValidationError;
+
+    @FindBy(css = "mat-dialog-actions button:nth-child(1)")
+    WebElement okBtn;
+
+    @FindBy(css = "mat-dialog-actions button:nth-child(2)")
+    WebElement cancelBtn;
+
+    @FindBy(css = "#changePassword_id")
+    WebElement changePassword;
+
+    @FindBy(css = "smp-password-change-dialog mat-dialog-actions button:nth-child(1)")
+    WebElement changedPassword;
+
+    @FindBy(css = "#closebuttondialog_id")
+    WebElement passChangedClose;
+
+    @FindBy(css = "smp-password-change-dialog mat-dialog-actions button:nth-child(2)")
+    WebElement passwordDialogClose;
+
+    @FindBy(css = "#role_id")
+    WebElement rolesSelectContainer;
+    public UserPopup(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+        rolesSelect = new GenericSelect(driver, rolesSelectContainer);
+    }
+
+    public boolean isAdminPasswordInputEnable() {
+        boolean bool = adminPassInput.isEnabled();
+        return bool;
+    }
+
+    public boolean isNewPasswordInputEnable() {
+        boolean bool = newPasswordInput.isEnabled();
+        return bool;
+    }
+
+    public boolean isConfirmPasswordInputEnable() {
+        boolean bool = confirmationInput.isEnabled();
+        return bool;
+    }
 
 
-	@FindBy(id = "userDetailsToggle_id")
-	WebElement userDetailsToggle;
+    public boolean isOKButtonActive() {
+        return isEnabled(okBtn);
+    }
 
-	@FindBy(css = "#active_id > label > div > div")
-	WebElement activeToggle;
+    public boolean isChangePasswordActive() {
+        return isEnabled(passChangedClose);
+    }
 
-	@FindBy(id = "username_id")
-	WebElement userNameInput;
+    public boolean isChangePasswordButtonActive() {
+        return isEnabled(changedPassword);
+    }
 
-	@FindBy(id = "emailAddress_id")
-	WebElement emailInput;
+    public boolean isCancelButtonActive() {
+        return isEnabled(cancelBtn);
+    }
 
-	@FindBy(id = "np_id")
-	WebElement newPasswordInput;
+    public void fillData(String user, String email, String role, String password, String confirmation) {
+        clearAndFillInput(userNameInput, user);
+        clearAndFillInput(emailInput, email);
+        clearAndFillInput(newPasswordInput, password);
+        clearAndFillInput(confirmationInput, confirmation);
 
-	@SuppressWarnings("SpellCheckingInspection")
-	@FindBy(id = "cnp_id")
-	WebElement confirmationInput;
+        GenericSelect rolesSelect = new GenericSelect(driver, rolesSelectContainer);
+        rolesSelect.selectOptionByText(role);
 
-	/*@FindBy(css = "input#cp_id")
-	WebElement adminPassInput;*/
+    }
 
-	@FindBy(xpath = "//input[@data-placeholder='Admin password for user [system]']")
-	WebElement adminPassInput;
+    public void clickOK() {
+        log.info("click OK");
+        waitForElementToBeClickable(okBtn);
+        okBtn.click();
+        waitForElementToBeGone(okBtn);
+    }
 
-	@FindBy(css = "mat-form-field.username> div > div.mat-form-field-flex > div > div")
-	WebElement usernameValidationError;
-
-	@FindBy(css = "smp-password-change-dialog .password-panel mat-form-field:nth-child(2) .mat-form-field-subscript-wrapper mat-error")
-	WebElement passValidationError;
-
-	@FindBy(css = "mat-form-field.password-confirmation > div > div.mat-form-field-flex > div > div")
-	WebElement passConfirmationValidationError;
-
-	@FindBy(css = ".ng-trigger.ng-trigger-transitionMessages.ng-star-inserted > mat-error")
-	WebElement passMatchValidationError;
-
-	@FindBy(css = "mat-dialog-actions button:nth-child(1)")
-	WebElement okBtn;
-
-	@FindBy(css = "mat-dialog-actions button:nth-child(2)")
-	WebElement cancelBtn;
-
-	@FindBy(css ="#changePassword_id")
-	WebElement changePassword;
-
-	@FindBy(css ="smp-password-change-dialog mat-dialog-actions button:nth-child(1)")
-	WebElement changedPassword;
-
-	@FindBy(css = "#closebuttondialog_id")
-	WebElement passChangedClose;
-
-	@FindBy(css = "smp-password-change-dialog mat-dialog-actions button:nth-child(2)")
-	WebElement passwordDialogClose;
-
-	@FindBy(css = "#role_id")
-	WebElement rolesSelectContainer;
-	public GenericSelect rolesSelect;
-
-	public boolean isAdminPasswordInputEnable()
-	{
-		boolean bool = adminPassInput.isEnabled();
-		return bool;
-	}
-	public boolean isNewPasswordInputEnable()
-	{
-		boolean bool = newPasswordInput.isEnabled();
-		return bool;
-	}
-	public boolean isConfirmPasswordInputEnable()
-	{
-		boolean bool = confirmationInput.isEnabled();
-		return bool;
-	}
+    public void clickCancel() {
+        log.info("click cancel");
+        waitForElementToBeClickable(cancelBtn);
+        cancelBtn.click();
+        waitForElementToBeGone(cancelBtn);
+    }
 
 
+    public void clickUserDetailsToggle() {
+        log.info("details toggle");
+        waitForElementToBeClickable(userDetailsToggle).click();
+        waitForElementToBeEnabled(userNameInput);
+    }
 
-	public boolean isOKButtonActive() {
-		return isEnabled(okBtn);
-	}
-	public boolean isChangePasswordActive() {
-		return isEnabled(passChangedClose);
-	}
-	public boolean isChangePasswordButtonActive() {
-		return isEnabled(changedPassword);
-	}
+    //public void fillDetailsForm(String username, String pass, String confirmation)
+    public void fillDetailsForm(String username) {
+        clearAndFillInput(userNameInput, username);
+        //clearAndFillInput(passwordInput, pass);
+        //clearAndFillInput(confirmationInput, confirmation);
+        emailInput.click();
+    }
 
-	public boolean isCancelButtonActive() {
-		return isEnabled(cancelBtn);
-	}
+    public void clickSetOrChangePassword() {
+        log.info("click change password");
+        waitForElementToBeClickable(changePassword);
+        changePassword.click();
+        waitForXMillis(1000);
+    }
 
-	public void fillData(String user, String email, String role, String password, String confirmation) {
-		clearAndFillInput(userNameInput, user);
-		clearAndFillInput(emailInput, email);
-		clearAndFillInput(newPasswordInput, password);
-		clearAndFillInput(confirmationInput, confirmation);
+    public void clickCloseAfterChangedPass() {
+        log.info("click close after change password");
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        waitForElementToBeClickable(passChangedClose);
+        log.info("ab");
+        passChangedClose.click();
+        log.info("bd");
+        waitForElementToBeGone(passChangedClose);
+        log.info("cd");
+    }
 
-		GenericSelect rolesSelect = new GenericSelect(driver, rolesSelectContainer);
-		rolesSelect.selectOptionByText(role);
+    public ConfirmationDialog clickChangedPassword() {
+        log.info("click changed password");
+        waitForElementToBeClickable(changedPassword);
+        changedPassword.click();
+        waitForElementToBeGone(changedPassword);
+        return new ConfirmationDialog(driver);
+    }
 
-	}
+    public boolean isPopupChangedPasswordEnabled() {
+        return changedPassword.isEnabled();
+    }
 
-	public void clickOK() {
-		log.info("click OK");
-		waitForElementToBeClickable(okBtn);
-		okBtn.click();
-		waitForElementToBeGone(okBtn);
-	}
+    public void setOrChangePassword(String adminPass, String newPass, String confirmPass) {
 
-	public void clickCancel() {
-		log.info("click cancel");
-		waitForElementToBeClickable(cancelBtn);
-		cancelBtn.click();
-		waitForElementToBeGone(cancelBtn);
-	}
+        clearAndFillInput(adminPassInput, adminPass);
+        clearAndFillInput(newPasswordInput, newPass);
+        clearAndFillInput(confirmationInput, confirmPass);
+    }
 
-
-	public void clickUserDetailsToggle() {
-		log.info("details toggle");
-		waitForElementToBeClickable(userDetailsToggle).click();
-		waitForElementToBeEnabled(userNameInput);
-	}
-
-	//public void fillDetailsForm(String username, String pass, String confirmation)
-	public void fillDetailsForm(String username){
-		clearAndFillInput(userNameInput, username);
-		//clearAndFillInput(passwordInput, pass);
-		//clearAndFillInput(confirmationInput, confirmation);
-		emailInput.click();
-	}
-	public void clickSetOrChangePassword(){
-		log.info("click change password");
-		waitForElementToBeClickable(changePassword);
-		changePassword.click();
-		//waitForElementToBeGone(changePassword);
-	}
-	public void clickCloseAfterChangedPass()
-	{
-		log.info("click close after change password");
-		try {
-			Thread.sleep(10000);
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
-		waitForElementToBeClickable(passChangedClose);
-		log.info("ab");
-		passChangedClose.click();
-		log.info("bd");
-		waitForElementToBeGone(passChangedClose);
-		log.info("cd");
-	}
-	public ConfirmationDialog clickChangedPassword()
-	{
-		log.info("click changed password");
-		waitForElementToBeClickable(changedPassword);
-		changedPassword.click();
-		waitForElementToBeGone(changedPassword);
-		return new ConfirmationDialog(driver);
-	}
-	public boolean isPopupChangedPasswordEnabled()
-	{
-		return changedPassword.isEnabled();
-	}
-	public void setOrChangePassword(String adminPass,String newPass,String confirmPass)
-	{
-
-		clearAndFillInput(adminPassInput,adminPass);
-		clearAndFillInput(newPasswordInput,newPass);
-		clearAndFillInput(confirmationInput,confirmPass);
-	}
-	public void clickClosePasswordDialog()
-	{
-		passwordDialogClose.click();
-		waitForElementToBeGone(passwordDialogClose);
-	}
+    public void clickClosePasswordDialog() {
+        passwordDialogClose.click();
+        waitForElementToBeGone(passwordDialogClose);
+    }
 
 
-	public String getUsernameValidationError() {
-		try {
-			waitForElementToBeVisible(usernameValidationError);
-			return usernameValidationError.getText();
-		} catch (Exception e) {
-		}
-		return null;
-	}
+    public String getUsernameValidationError() {
+        try {
+            waitForElementToBeVisible(usernameValidationError);
+            return usernameValidationError.getText();
+        } catch (Exception e) {
+        }
+        return null;
+    }
 
-	public String getPassValidationError() {
-		try {
-			waitForElementToBeVisible(passValidationError);
-			return passValidationError.getText();
-		} catch (Exception e) {
-		}
-		return null;
-	}
+    public String getPassValidationError() {
+        try {
+            waitForElementToBeVisible(passValidationError);
+            return passValidationError.getText();
+        } catch (Exception e) {
+        }
+        return null;
+    }
 
-	public String getConfirmationPassValidationError() {
+    public String getConfirmationPassValidationError() {
 
-		try {
-			waitForElementToBeVisible(passConfirmationValidationError);
-			return passConfirmationValidationError.getText();
-		} catch (Exception e) {
-		}
-		return null;
-	}
+        try {
+            waitForElementToBeVisible(passConfirmationValidationError);
+            return passConfirmationValidationError.getText();
+        } catch (Exception e) {
+        }
+        return null;
+    }
 
-	public boolean isDuplicateUserNameErrorMsgDisPlayed() {
-		try {
-			return driver.findElement(By.cssSelector("mat-form-field.username > div .has-error")).isDisplayed();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public boolean isDuplicateUserNameErrorMsgDisPlayed() {
+        try {
+            return driver.findElement(By.cssSelector("mat-form-field.username > div .has-error")).isDisplayed();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public String getPassDontMatchValidationMsg() {
-		//WebElement passwordUnmatchingMsg = driver.findElement(By.cssSelector(".ng-trigger.ng-trigger-transitionMessages.ng-star-inserted > mat-error"));
-		return passMatchValidationError.getText();
-		//".mat-form-field-infix > div.has-error"
-	}
+    public String getPassDontMatchValidationMsg() {
+        //WebElement passwordUnmatchingMsg = driver.findElement(By.cssSelector(".ng-trigger.ng-trigger-transitionMessages.ng-star-inserted > mat-error"));
+        return passMatchValidationError.getText();
+        //".mat-form-field-infix > div.has-error"
+    }
 }
--- smp-ui-tests/src/test/java/ui/BaseTest.java
+++ smp-ui-tests/src/test/java/ui/BaseTest.java
@@ -21,186 +21,173 @@ import java.util.List;
 @Listeners({ExcelTestReporter.class, TestProgressReporter.class})
 public class BaseTest {
 
-	static int methodCount = 1;
+    static int methodCount = 1;
 
-	static WebDriver driver;
-	protected Logger logger = Logger.getLogger(this.getClass());
-	static TestDataProvider testDataProvider = new TestDataProvider();
+    static WebDriver driver;
+    static TestDataProvider testDataProvider = new TestDataProvider();
+    static ArrayList<String> createdDomains = new ArrayList<>();
+    static ArrayList<String> createdUsers = new ArrayList<>();
+    static ArrayList<String> createdServiceGroups = new ArrayList<>();
+    protected Logger logger = Logger.getLogger(this.getClass());
 
-	static ArrayList<String> createdDomains = new ArrayList<>();
-	static ArrayList<String> createdUsers = new ArrayList<>();
-	static ArrayList<String> createdServiceGroups = new ArrayList<>();
-
-	@BeforeSuite(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
 	/*Starts the browser and navigates to the homepage. This happens once before the test
 	suite and the browser window is reused for all tests in suite*/
-	public void beforeSuite() {
-		logger.info("Creating necessary data !!!!");
-		createDomains();
-		createUsers();
-		createSGs();
-
-//		logger.info("Starting this puppy!!!!");
-//		driver = DriverManager.getDriver();
-//		driver.get(PROPERTIES.UI_BASE_URL);
-	}
+    public void beforeSuite() {
+        logger.info("Creating necessary data !!!!");
+        createDomains();
+        createUsers();
+        createSGs();
+    }
 
 
-	@AfterSuite(alwaysRun = true)
-	/*After the test suite is done we close the browser*/
-	public void afterSuite() {
-		logger.info("Deleting created data!!!");
+    @AfterSuite(alwaysRun = true)
+    /*After the test suite is done we close the browser*/
+    public void afterSuite() {
+        logger.info("Deleting created data!!!");
 
-		deleteTestData();
+        deleteTestData();
 
-		logger.info("Quitting!!!! Buh bye!!!");
-		try {
-			if(null!=driver){
-				driver.quit();
-			}
-		} catch (Exception e) {
-			logger.warn("Closing the driver failed !!!!");
-			e.printStackTrace();
-		}
-	}
+        logger.info("Quitting!!!! Buh bye!!!");
+        try {
+            if (null != driver) {
+                driver.quit();
+            }
+        } catch (Exception e) {
+            logger.warn("Closing the driver failed !!!!");
+            e.printStackTrace();
+        }
+    }
 
-	@AfterClass(alwaysRun = true)
-	public void afterClass() {
-		if(null!=driver){
-			driver.quit();
-		}
-//		driver.get(PROPERTIES.UI_BASE_URL);
-//		SMPPage page = new SMPPage(driver);
-//		page.refreshPage();
-//
-//		if(page.pageHeader.sandwichMenu.isLoggedIn()){
-//			logger.info("Logout!!");
-//			page.pageHeader.sandwichMenu.logout();
-//		}
-	}
+    @AfterClass(alwaysRun = true)
+    public void afterClass() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
 
-	@BeforeClass(alwaysRun = true)
-	public void beforeClass() {
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
         logger.info("beforeClass entry");
-		driver = DriverManager.getDriver();
-		driver.get(PROPERTIES.UI_BASE_URL);
+        driver = DriverManager.getDriver();
+        driver.get(PROPERTIES.UI_BASE_URL);
         logger.info("beforeClass exit");
-	}
+    }
 
-	@BeforeMethod(alwaysRun = true)
-	protected void logSeparator(Method method) throws Exception {
+    @BeforeMethod(alwaysRun = true)
+    protected void logSeparator(Method method) throws Exception {
 
-		logger.info("--------------------------- Running test number: " + methodCount);
-		logger.info("--------------------------- Running test method: " + method.getDeclaringClass().getSimpleName() + "." + method.getName());
-		methodCount++;
-	}
-
-
-	private void createDomains() {
-		for (int i = 0; i < 5; i++) {
-			String generated = Generator.randomAlphaNumeric(10);
-			logger.info("creating domain whose value is :"+generated);
-			boolean created = SMPRestClient.createDomain(generated);
-			if (created) {
-				createdDomains.add(generated);
-			} else {
-				logger.warn("Test data creation: Domain creation failed for " + generated);
-				System.exit(-1);
-			}
-		}
-	}
-
-	private void createUsers() {
-		String[] roles = {"SMP_ADMIN", "SERVICE_GROUP_ADMIN", "SYSTEM_ADMIN"};
-		for (int i = 0; i < 6; i++) {
-			String generated = Generator.randomAlphaNumeric(10);
-			String role = roles[i % roles.length];
-			boolean created = SMPRestClient.createUser(generated, role);
-			if (created) {
-				createdUsers.add(generated);
-			} else {
-				logger.warn("Test data creation: User creation failed for " + generated);
-				System.exit(-1);
-			}
-		}
-	}
-
-	private void createSGs() {
-		for (int i = 0; i < 5; i++) {
-			String generated = Generator.randomAlphaNumeric(10);
-			String generatedHyphen = generated.substring(0,3)+"-"+generated.substring(3,6)+"-"+generated.substring(6,9);
-			List<String> users = Arrays.asList(createdUsers.get(0));
-			List<String> domains = Arrays.asList(createdDomains.get(0));
-			boolean created = SMPRestClient.createServiceGroup(generated, generatedHyphen, users, domains);
-			if (created) {
-				createdServiceGroups.add(generated);
-			} else {
-				logger.warn("Test data creation: SG creation failed for " + generated);
-				System.exit(-1);
-			}
-		}
-	}
-
-	private void deleteTestData() {
-		for (String createdServiceGroup : createdServiceGroups) {
-			try {
-				SMPRestClient.deleteSG(createdServiceGroup);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		for (String createdUser : createdUsers) {
-			try {
-				SMPRestClient.deleteUser(createdUser);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		for (String createdDomain : createdDomains) {
-			try {
-				SMPRestClient.deleteDomain(createdDomain);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        logger.info("--------------------------- Running test number: " + methodCount);
+        logger.info("--------------------------- Running test method: " + method.getDeclaringClass().getSimpleName() + "." + method.getName());
+        methodCount++;
+    }
 
 
-	protected void genericLogoutProcedure() {
-		logger.info("executing the generic logout procedure");
+    private void createDomains() {
+        for (int i = 0; i < 5; i++) {
+            String generated = Generator.randomAlphaNumeric(10);
+            logger.info("creating domain whose value is :" + generated);
+            boolean created = SMPRestClient.createDomain(generated);
+            if (created) {
+                createdDomains.add(generated);
+            } else {
+                logger.warn("Test data creation: Domain creation failed for " + generated);
+                System.exit(-1);
+            }
+        }
+    }
 
-		SMPPage page = new SMPPage(driver);
-		page.refreshPage();
+    private void createUsers() {
+        String[] roles = {"SMP_ADMIN", "SERVICE_GROUP_ADMIN", "SYSTEM_ADMIN"};
+        for (int i = 0; i < 6; i++) {
+            String generated = Generator.randomAlphaNumeric(10);
+            String role = roles[i % roles.length];
+            boolean created = SMPRestClient.createUser(generated, role);
+            if (created) {
+                createdUsers.add(generated);
+            } else {
+                logger.warn("Test data creation: User creation failed for " + generated);
+                System.exit(-1);
+            }
+        }
+    }
 
-		try {
-			if (page.pageHeader.sandwichMenu.isLoggedIn()) {
-				logger.info("Logout!!");
-				page.pageHeader.sandwichMenu.logout();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    private void createSGs() {
+        for (int i = 0; i < 5; i++) {
+            String generated = Generator.randomAlphaNumeric(10);
+            String generatedHyphen = generated.substring(0, 3) + "-" + generated.substring(3, 6) + "-" + generated.substring(6, 9);
+            List<String> users = Arrays.asList(createdUsers.get(0));
+            List<String> domains = Arrays.asList(createdDomains.get(0));
+            boolean created = SMPRestClient.createServiceGroup(generated, generatedHyphen, users, domains);
+            if (created) {
+                createdServiceGroups.add(generated);
+            } else {
+                logger.warn("Test data creation: SG creation failed for " + generated);
+                System.exit(-1);
+            }
+        }
+    }
 
-		driver.manage().deleteAllCookies();
-		((JavascriptExecutor) driver).executeScript("localStorage.clear();");
+    private void deleteTestData() {
+        for (String createdServiceGroup : createdServiceGroups) {
+            try {
+                SMPRestClient.deleteSG(createdServiceGroup);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        for (String createdUser : createdUsers) {
+            try {
+                SMPRestClient.deleteUser(createdUser);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        for (String createdDomain : createdDomains) {
+            try {
+                SMPRestClient.deleteDomain(createdDomain);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		page.refreshPage();
-		page.waitForXMillis(100);
-	}
 
-	protected SMPPage genericLoginProcedure(String role) {
-		SMPPage page = new SMPPage(driver);
+    protected void genericLogoutProcedure() {
+        logger.info("executing the generic logout procedure");
 
-		genericLogoutProcedure();
+        SMPPage page = new SMPPage(driver);
+        page.refreshPage();
 
-		if (!page.pageHeader.sandwichMenu.isLoggedIn()) {
-			logger.info("Login!!");
-			page.pageHeader.goToLogin().login(role);
-		}
+        try {
+            if (page.pageHeader.sandwichMenu.isLoggedIn()) {
+                logger.info("Logout!!");
+                page.pageHeader.sandwichMenu.logout();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		page.waitForRowsToLoad();
-		return page;
-	}
+        driver.manage().deleteAllCookies();
+        ((JavascriptExecutor) driver).executeScript("localStorage.clear();");
+
+        page.refreshPage();
+        page.waitForXMillis(100);
+    }
+
+    protected SMPPage genericLoginProcedure(String role) {
+        SMPPage page = new SMPPage(driver);
+
+        genericLogoutProcedure();
+
+        if (!page.pageHeader.sandwichMenu.isLoggedIn()) {
+            logger.info("Login!!");
+            page.pageHeader.goToLogin().login(role);
+        }
+
+        page.waitForRowsToLoad();
+        return page;
+    }
 
 }
 
