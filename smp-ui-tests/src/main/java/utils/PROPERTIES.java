0070d65a5d18ec58b363b8ba3b6fb6d4a34bde59
Catalin Comanici <Catalin.COMANICI@ext.ec.europa.eu>
2022-10-10 11:13:21 +0200

    added screenshots on test failure, SUT info, and other minor fixes

--- smp-ui-tests/src/main/java/utils/PROPERTIES.java
+++ smp-ui-tests/src/main/java/utils/PROPERTIES.java
@@ -7,8 +7,7 @@ public class PROPERTIES {
 	public static final int SHORT_UI_TIMEOUT = 5;
 	public static final int TIMEOUT = Integer.valueOf(System.getProperty("SHORT_TIMEOUT"));
 
-	public static final String StringHEADLESS = System.getProperty("HEADLESS");
-	public static final String HEADLESS = "false";
+	public static final String HEADLESS = "true";
 	public static final String REPORTS_FOLDER = System.getProperty("reports.folder");
 	public static final String TESTDATAFILE = System.getProperty("data.folder") + System.getProperty("testdata.file");
 
--- smp-ui-tests/src/test/java/ui/BaseTest.java
+++ smp-ui-tests/src/test/java/ui/BaseTest.java
@@ -1,5 +1,7 @@
 package ui;
 
+import customReporter.ExcelTestReporter;
+import customReporter.TestProgressReporter;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -9,8 +11,6 @@ import utils.DriverManager;
 import utils.Generator;
 import utils.PROPERTIES;
 import utils.TestDataProvider;
-import utils.customReporter.ExcelTestReporter;
-import utils.customReporter.TestProgressReporter;
 import utils.rest.SMPRestClient;
 
 import java.lang.reflect.Method;
@@ -21,171 +21,177 @@ import java.util.List;
 @Listeners({ExcelTestReporter.class, TestProgressReporter.class})
 public class BaseTest {
 
-    static int methodCount = 1;
+	public static WebDriver driver;
+	static int methodCount = 1;
+	static TestDataProvider testDataProvider = new TestDataProvider();
+	static ArrayList<String> createdDomains = new ArrayList<>();
+	static ArrayList<String> createdUsers = new ArrayList<>();
+	static ArrayList<String> createdServiceGroups = new ArrayList<>();
+	protected Logger logger = Logger.getLogger(this.getClass());
 
-    static WebDriver driver;
-    static TestDataProvider testDataProvider = new TestDataProvider();
-    static ArrayList<String> createdDomains = new ArrayList<>();
-    static ArrayList<String> createdUsers = new ArrayList<>();
-    static ArrayList<String> createdServiceGroups = new ArrayList<>();
-    protected Logger logger = Logger.getLogger(this.getClass());
+	@BeforeSuite(alwaysRun = true)
+	public void beforeSuite() {
 
-    @BeforeSuite(alwaysRun = true)
-    public void beforeSuite() {
-        logger.info("Creating necessary data !!!!");
-        createDomains();
-        createUsers();
-        createSGs();
-    }
+		logger.info("********************** SUT Info **********************");
 
+		System.out.println(SMPRestClient.getSystemInfo());
 
-    @AfterSuite(alwaysRun = true)
-    /*After the test suite is done, we close the browser*/
-    public void afterSuite() {
-        logger.info("Deleting created data!!!");
+		logger.info("********************** SUT Info **********************");
 
-        deleteTestData();
-
-        logger.info("Quitting!!!! Buh bye!!!");
-        try {
-            if (null != driver) {
-                driver.quit();
-            }
-        } catch (Exception e) {
-            logger.warn("Closing the driver failed !!!!");
-            e.printStackTrace();
-        }
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void afterClass() {
-        if (null != driver) {
-            driver.quit();
-        }
-    }
-
-    @BeforeClass(alwaysRun = true)
-    public void beforeClass() {
-        logger.info("beforeClass entry");
-        driver = DriverManager.getDriver();
-        driver.get(PROPERTIES.UI_BASE_URL);
-        logger.info("beforeClass exit");
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    protected void logSeparator(Method method) throws Exception {
-
-        logger.info("--------------------------- Running test number: " + methodCount);
-        logger.info("--------------------------- Running test method: " + method.getDeclaringClass().getSimpleName() + "." + method.getName());
-        methodCount++;
-    }
+		logger.info("Creating necessary data !!!!");
+		createDomains();
+		createUsers();
+		createSGs();
+	}
 
 
-    private void createDomains() {
-        for (int i = 0; i < 5; i++) {
-            String generated = Generator.randomAlphaNumeric(10);
-            logger.info("creating domain whose value is :" + generated);
-            boolean created = SMPRestClient.createDomain(generated);
-            if (created) {
-                createdDomains.add(generated);
-            } else {
-                logger.warn("Test data creation: Domain creation failed for " + generated);
-                System.exit(-1);
-            }
-        }
-    }
+	@AfterSuite(alwaysRun = true)
+	/*After the test suite is done, we close the browser*/
+	public void afterSuite() {
+		logger.info("Deleting created data!!!");
 
-    private void createUsers() {
-        String[] roles = {"SMP_ADMIN", "SERVICE_GROUP_ADMIN", "SYSTEM_ADMIN"};
-        for (int i = 0; i < 6; i++) {
-            String generated = Generator.randomAlphaNumeric(10);
-            String role = roles[i % roles.length];
-            boolean created = SMPRestClient.createUser(generated, role);
-            if (created) {
-                createdUsers.add(generated);
-            } else {
-                logger.warn("Test data creation: User creation failed for " + generated);
-                System.exit(-1);
-            }
-        }
-    }
+		deleteTestData();
 
-    private void createSGs() {
-        for (int i = 0; i < 5; i++) {
-            String generated = Generator.randomAlphaNumeric(10);
-            String generatedHyphen = generated.substring(0, 3) + "-" + generated.substring(3, 6) + "-" + generated.substring(6, 9);
-            List<String> users = Arrays.asList(createdUsers.get(0));
-            List<String> domains = Arrays.asList(createdDomains.get(0));
-            boolean created = SMPRestClient.createServiceGroup(generated, generatedHyphen, users, domains);
-            if (created) {
-                createdServiceGroups.add(generated);
-            } else {
-                logger.warn("Test data creation: SG creation failed for " + generated);
-                System.exit(-1);
-            }
-        }
-    }
+		logger.info("Quitting!!!! Buh bye!!!");
+		try {
+			if (null != driver) {
+				driver.quit();
+			}
+		} catch (Exception e) {
+			logger.warn("Closing the driver failed !!!!");
+			e.printStackTrace();
+		}
+	}
 
-    private void deleteTestData() {
-        for (String createdServiceGroup : createdServiceGroups) {
-            try {
-                SMPRestClient.deleteSG(createdServiceGroup);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        for (String createdUser : createdUsers) {
-            try {
-                SMPRestClient.deleteUser(createdUser);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        for (String createdDomain : createdDomains) {
-            try {
-                SMPRestClient.deleteDomain(createdDomain);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	@AfterClass(alwaysRun = true)
+	public void afterClass() {
+		if (null != driver) {
+			driver.quit();
+		}
+	}
+
+	@BeforeClass(alwaysRun = true)
+	public void beforeClass() {
+		logger.info("beforeClass entry");
+		driver = DriverManager.getDriver();
+		driver.get(PROPERTIES.UI_BASE_URL);
+		logger.info("beforeClass exit");
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	protected void logSeparator(Method method) throws Exception {
+
+		logger.info("--------------------------- Running test number: " + methodCount);
+		logger.info("--------------------------- Running test method: " + method.getDeclaringClass().getSimpleName() + "." + method.getName());
+		methodCount++;
+	}
 
 
-    protected void genericLogoutProcedure() {
-        logger.info("executing the generic logout procedure");
+	private void createDomains() {
+		for (int i = 0; i < 5; i++) {
+			String generated = Generator.randomAlphaNumeric(10);
+			logger.info("creating domain whose value is :" + generated);
+			boolean created = SMPRestClient.createDomain(generated);
+			if (created) {
+				createdDomains.add(generated);
+			} else {
+				logger.warn("Test data creation: Domain creation failed for " + generated);
+				System.exit(-1);
+			}
+		}
+	}
 
-        SMPPage page = new SMPPage(driver);
-        page.refreshPage();
+	private void createUsers() {
+		String[] roles = {"SMP_ADMIN", "SERVICE_GROUP_ADMIN", "SYSTEM_ADMIN"};
+		for (int i = 0; i < 6; i++) {
+			String generated = Generator.randomAlphaNumeric(10);
+			String role = roles[i % roles.length];
+			boolean created = SMPRestClient.createUser(generated, role);
+			if (created) {
+				createdUsers.add(generated);
+			} else {
+				logger.warn("Test data creation: User creation failed for " + generated);
+				System.exit(-1);
+			}
+		}
+	}
 
-        try {
-            if (page.pageHeader.sandwichMenu.isLoggedIn()) {
-                logger.info("Logout!!");
-                page.pageHeader.sandwichMenu.logout();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	private void createSGs() {
+		for (int i = 0; i < 5; i++) {
+			String generated = Generator.randomAlphaNumeric(10);
+			String generatedHyphen = generated.substring(0, 3) + "-" + generated.substring(3, 6) + "-" + generated.substring(6, 9);
+			List<String> users = Arrays.asList(createdUsers.get(0));
+			List<String> domains = Arrays.asList(createdDomains.get(0));
+			boolean created = SMPRestClient.createServiceGroup(generated, generatedHyphen, users, domains);
+			if (created) {
+				createdServiceGroups.add(generated);
+			} else {
+				logger.warn("Test data creation: SG creation failed for " + generated);
+				System.exit(-1);
+			}
+		}
+	}
 
-        driver.manage().deleteAllCookies();
-        ((JavascriptExecutor) driver).executeScript("localStorage.clear();");
+	private void deleteTestData() {
+		for (String createdServiceGroup : createdServiceGroups) {
+			try {
+				SMPRestClient.deleteSG(createdServiceGroup);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		for (String createdUser : createdUsers) {
+			try {
+				SMPRestClient.deleteUser(createdUser);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		for (String createdDomain : createdDomains) {
+			try {
+				SMPRestClient.deleteDomain(createdDomain);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-        page.refreshPage();
-        page.waitForXMillis(100);
-    }
 
-    protected SMPPage genericLoginProcedure(String role) {
-        SMPPage page = new SMPPage(driver);
+	protected void genericLogoutProcedure() {
+		logger.info("executing the generic logout procedure");
 
-        genericLogoutProcedure();
+		SMPPage page = new SMPPage(driver);
+		page.refreshPage();
 
-        if (!page.pageHeader.sandwichMenu.isLoggedIn()) {
-            logger.info("Login!!");
-            page.pageHeader.goToLogin().login(role);
-        }
+		try {
+			if (page.pageHeader.sandwichMenu.isLoggedIn()) {
+				logger.info("Logout!!");
+				page.pageHeader.sandwichMenu.logout();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        page.waitForRowsToLoad();
-        return page;
-    }
+		driver.manage().deleteAllCookies();
+		((JavascriptExecutor) driver).executeScript("localStorage.clear();");
+
+		page.refreshPage();
+		page.waitForXMillis(100);
+	}
+
+	protected SMPPage genericLoginProcedure(String role) {
+		SMPPage page = new SMPPage(driver);
+
+		genericLogoutProcedure();
+
+		if (!page.pageHeader.sandwichMenu.isLoggedIn()) {
+			logger.info("Login!!");
+			page.pageHeader.goToLogin().login(role);
+		}
+
+		page.waitForRowsToLoad();
+		return page;
+	}
 
 }
 
