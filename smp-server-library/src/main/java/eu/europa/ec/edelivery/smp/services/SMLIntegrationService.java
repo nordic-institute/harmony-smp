a2f6c56fa67e2927335d82719b02f862b8c1a0b8
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2023-04-25 13:10:24 +0200

    Fix edit group domain change not refreshing groups

--- smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/services/SMLIntegrationService.java
+++ smp-server-library/src/main/java/eu/europa/ec/edelivery/smp/services/SMLIntegrationService.java
@@ -94,8 +94,7 @@ public class SMLIntegrationService {
         LOG.businessDebug(BUS_SML_REGISTER_SERVICE_GROUP, resource.getIdentifierValue(), resource.getIdentifierScheme(), domain.getDomainCode());
         if (!isSMLIntegrationEnabled()) {
             String msg = "SML integration is not enabled!";
-            LOG.businessError(BUS_SML_REGISTER_SERVICE_GROUP_FAILED, resource.getIdentifierValue(), resource.getIdentifierScheme(), domain.getDomainCode(), msg);
-            throw new SMPRuntimeException(CONFIGURATION_ERROR, msg);
+            LOG.businessWarn(BUS_SML_REGISTER_SERVICE_GROUP_FAILED, resource.getIdentifierValue(), resource.getIdentifierScheme(), domain.getDomainCode(), msg);
         }
         Identifier normalizedParticipantId = identifierService
                 .normalizeParticipant(resource.getIdentifierScheme(), resource.getIdentifierValue());
