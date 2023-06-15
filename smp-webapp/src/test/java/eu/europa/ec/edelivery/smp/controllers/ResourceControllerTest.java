e74b883522b1c27880d0a67144f7f51b306bc0f2
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2023-06-15 14:00:03 +0200

    UI fixes (group deletion) and update unit tests

--- smp-webapp/src/test/java/eu/europa/ec/edelivery/smp/controllers/ResourceControllerTest.java
+++ smp-webapp/src/test/java/eu/europa/ec/edelivery/smp/controllers/ResourceControllerTest.java
@@ -15,28 +15,36 @@ package eu.europa.ec.edelivery.smp.controllers;
 
 import eu.europa.ec.edelivery.smp.data.dao.ConfigurationDao;
 import eu.europa.ec.edelivery.smp.test.SmpTestWebAppConfig;
-import eu.europa.ec.edelivery.smp.test.testutils.MockMvcUtils;
 import eu.europa.ec.edelivery.smp.test.testutils.X509CertificateTestUtils;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.UUID;
 
-import static eu.europa.ec.edelivery.smp.ServiceGroupBodyUtil.*;
+import static eu.europa.ec.edelivery.smp.ServiceGroupBodyUtil.generateServiceMetadata;
+import static eu.europa.ec.edelivery.smp.ServiceGroupBodyUtil.getSampleServiceGroupBody;
 import static java.lang.String.format;
 import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
@@ -52,14 +60,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {SmpTestWebAppConfig.class})
-@Ignore
+@Sql(scripts = {
+        "classpath:/cleanup-database.sql",
+        "classpath:/webapp_integration_test_data.sql"},
+        executionPhase = BEFORE_TEST_METHOD)
 public class ResourceControllerTest {
 
-    private static final String IDENTIFIER_SCHEME = "ehealth-participantid-qns";
-    private static final String PARTICIPANT_ID = "urn:test:resource:01";
+    public static final Logger LOG = LoggerFactory.getLogger(ResourceControllerTest.class);
 
+    private static final String IDENTIFIER_SCHEME = "ehealth-participantid-qns";
     private static final String DOCUMENT_SCHEME = "doctype";
-    private static final String DOCUMENT_ID = "invoice:01";
 
     private static final String HTTP_HEADER_KEY_DOMAIN = "Domain";
     private static final String HTTP_HEADER_KEY_SERVICE_GROUP_OWNER = "ServiceGroup-Owner";
@@ -83,16 +93,24 @@ public class ResourceControllerTest {
 
     @Before
     public void setup() throws IOException {
-        forwardedHeaderTransformer.setRemoveOnly(false);
         X509CertificateTestUtils.reloadKeystores();
-        mvc = MockMvcUtils.initializeMockMvc(webAppContext);
-        configurationDao.reloadPropertiesFromDatabase();
+        mvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
 
+        initServletContext();
+    }
+
+    private void initServletContext() {
+        MockServletContext sc = new MockServletContext("");
+        ServletContextListener listener = new ContextLoaderListener(webAppContext);
+        ServletContextEvent event = new ServletContextEvent(sc);
+        listener.contextInitialized(event);
     }
 
     @Test
     public void notFoundIsReturnedWhenServiceGroupDoesNotExist() throws Exception {
-        mvc.perform(get(format("/%s::%s", IDENTIFIER_SCHEME, PARTICIPANT_ID)))
+        mvc.perform(get(format("/%s::%s", IDENTIFIER_SCHEME, UUID.randomUUID().toString())))
                 .andExpect(status().isNotFound());
     }
 
@@ -168,11 +186,12 @@ public class ResourceControllerTest {
     public void existingServiceMetadataCanBeRetrievedByEverybody() throws Exception {
 
         String participantId = UUID.randomUUID().toString();
+        String documentId = UUID.randomUUID().toString();
         String resourceExample = getSampleServiceGroupBody(IDENTIFIER_SCHEME, participantId);
         String urlPath = format("/%s::%s", IDENTIFIER_SCHEME, participantId);
-        String docUrlPath = format("%s/services/%s::%s", urlPath, DOCUMENT_SCHEME, DOCUMENT_ID);
+        String docUrlPath = format("%s/services/%s::%s", urlPath, DOCUMENT_SCHEME, documentId);
 
-        String xmlMD = generateServiceMetadata(participantId, IDENTIFIER_SCHEME, DOCUMENT_ID, DOCUMENT_SCHEME, "test");
+        String xmlMD = generateServiceMetadata(participantId, IDENTIFIER_SCHEME, documentId, DOCUMENT_SCHEME, "test");
         // crate service group
         mvc.perform(put(urlPath)
                 .with(ADMIN_CREDENTIALS)
@@ -190,14 +209,14 @@ public class ResourceControllerTest {
                 .andExpect(status().isCreated());
 
         mvc.perform(get(urlPath))
-                .andExpect(content().xml(generateExpectedServiceGroup("http://localhost/", IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup("http://localhost/", IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
 
         mvc.perform(get(docUrlPath))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void getExistingServiceMetadatWithReverseProxyHost() throws Exception {
+    public void getExistingServiceMetadataWithReverseProxyHost() throws Exception {
         //given
         String participantId = UUID.randomUUID().toString();
         String documentId = UUID.randomUUID().toString();
@@ -210,7 +229,7 @@ public class ResourceControllerTest {
                 .header("X-Forwarded-Host", "ec.test.eu")
                 .header("X-Forwarded-Port", "")
                 .header("X-Forwarded-Proto", "http"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
@@ -227,10 +246,11 @@ public class ResourceControllerTest {
         mvc.perform(get(urlPath)
                 .header("X-Forwarded-Port", "")
                 .header("X-Forwarded-Proto", "http"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
+    @Ignore
     public void getExistingServiceMetadataWithReverseProxyPort() throws Exception {
         //given
         String participantId = UUID.randomUUID().toString();
@@ -239,11 +259,11 @@ public class ResourceControllerTest {
         prepareForGet(participantId,documentId);
 
         // when then..
-        String expectedUrl = "http://ec.test.eu:8443/";
+        String expectedUrl = "https://ec.test.eu:8443/";
         mvc.perform(get(urlPath)
                 .header("X-Forwarded-Host", "ec.test.eu:8443")
-                .header("X-Forwarded-Proto", "http"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .header("X-Forwarded-Proto", "https"))
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
@@ -260,7 +280,7 @@ public class ResourceControllerTest {
                 .header("X-Forwarded-Port", "8443")
                 .header("X-Forwarded-Host", "ec.test.eu")
                 .header("X-Forwarded-Proto", "https"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
@@ -269,6 +289,8 @@ public class ResourceControllerTest {
         String participantId = UUID.randomUUID().toString();
         String documentId = UUID.randomUUID().toString();
         String urlPath = format("/%s::%s", IDENTIFIER_SCHEME, participantId);
+
+        LOG.info("Create service metadata: getExistingServiceMetadataWithReverseProxySkipDefaultPortHttps [{}]", urlPath);
         prepareForGet(participantId,documentId);
 
         // when then..
@@ -277,7 +299,7 @@ public class ResourceControllerTest {
                 .header("X-Forwarded-Port", "443")
                 .header("X-Forwarded-Host", "ec.test.eu")
                 .header("X-Forwarded-Proto", "https"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
@@ -294,10 +316,11 @@ public class ResourceControllerTest {
                 .header("X-Forwarded-Port", "80")
                 .header("X-Forwarded-Host", "ec.test.eu")
                 .header("X-Forwarded-Proto", "http"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     @Test
+    @Ignore
     public void getExistingServiceMetadataWithReverseProxyPortInHost() throws Exception {
         //given
         String participantId = UUID.randomUUID().toString();
@@ -311,7 +334,7 @@ public class ResourceControllerTest {
                 .header("X-Forwarded-Port", "8443")
                 .header("X-Forwarded-Host", "ec.test.eu:8443")
                 .header("X-Forwarded-Proto", "https"))
-                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, DOCUMENT_ID)));
+                .andExpect(content().xml(generateExpectedServiceGroup(expectedUrl, IDENTIFIER_SCHEME, participantId, DOCUMENT_SCHEME, documentId)));
     }
 
     public String generateExpectedServiceGroup(String expectedUrl, String resourceScheme, String resourceValue, String subresourceScheme, String subresourceValue) {
@@ -465,15 +488,17 @@ public class ResourceControllerTest {
         String xmlSG = getSampleServiceGroupBody(IDENTIFIER_SCHEME, participantId);
         String xmlMD = generateServiceMetadata(participantId, IDENTIFIER_SCHEME, documentId, DOCUMENT_SCHEME, "test");
         // crate service group
-        mvc.perform(put(urlPath)
 
+        LOG.info("create service service group: [{}]", docUrlPath);
+        mvc.perform(put(urlPath)
                 .header(HTTP_HEADER_KEY_DOMAIN, HTTP_DOMAIN_VALUE)
                 .with(ADMIN_CREDENTIALS)
                 .contentType(APPLICATION_XML_VALUE)
                 .content(xmlSG))
                 .andExpect(status().isCreated());
         // add service metadata
-        mvc.perform(put(docUrlPath)
+        LOG.info("create service metadata: [{}]", docUrlPath);
+        ResultActions actions = mvc.perform(put(docUrlPath)
                 .header(HTTP_HEADER_KEY_DOMAIN, HTTP_DOMAIN_VALUE)
                 .with(ADMIN_CREDENTIALS)
                 .contentType(APPLICATION_XML_VALUE)
