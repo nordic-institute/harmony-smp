a061bbfb1d1b98d29e955c4d26adaaa62c31be2a
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2023-06-16 08:47:38 +0200

    add signature to cpp document

--- smp-resource-extensions/oasis-cppa3-spi/src/test/java/eu/europa/ec/smp/spi/handler/OasisCppa3CppHandlerTest.java
+++ smp-resource-extensions/oasis-cppa3-spi/src/test/java/eu/europa/ec/smp/spi/handler/OasisCppa3CppHandlerTest.java
@@ -2,6 +2,7 @@ package eu.europa.ec.smp.spi.handler;
 
 import eu.europa.ec.smp.spi.api.SmpDataServiceApi;
 import eu.europa.ec.smp.spi.api.SmpIdentifierServiceApi;
+import eu.europa.ec.smp.spi.api.SmpXmlSignatureApi;
 import eu.europa.ec.smp.spi.api.model.RequestData;
 import eu.europa.ec.smp.spi.api.model.ResourceIdentifier;
 import eu.europa.ec.smp.spi.api.model.ResponseData;
@@ -9,6 +10,7 @@ import eu.europa.ec.smp.spi.exceptions.ResourceException;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.xml.sax.SAXParseException;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -21,7 +23,8 @@ public class OasisCppa3CppHandlerTest {
 
     SmpDataServiceApi smpDataApi = Mockito.mock(SmpDataServiceApi.class);
     SmpIdentifierServiceApi smpIdentifierServiceApi = Mockito.mock(SmpIdentifierServiceApi.class);
-    OasisCppa3CppHandler testInstance = new OasisCppa3CppHandler(smpDataApi, smpIdentifierServiceApi);
+    SmpXmlSignatureApi smpXmlSignatureApi = Mockito.mock(SmpXmlSignatureApi.class);
+    OasisCppa3CppHandler testInstance = new OasisCppa3CppHandler(smpDataApi, smpIdentifierServiceApi, smpXmlSignatureApi);
 
 
     RequestData requestData =  Mockito.mock(RequestData.class);
@@ -47,8 +50,16 @@ public class OasisCppa3CppHandlerTest {
         Mockito.doReturn(resourceIdentifier).when(smpIdentifierServiceApi).normalizeResourceIdentifier(Mockito.anyString(),Mockito.anyString());
 
         testInstance.validateResource(requestData);
+    }
 
+    @Test
+    public void validateOasisCPPASchema() throws ResourceException {
+        OasisCppa3CppHandler.validateOasisCPPASchema(OasisCppa3CppHandlerTest.class.getResourceAsStream("/examples/signed-cpp.xml"));
+    }
+    @Test
+    public void validateOasisCPPASchemaInvalid()  {
 
-
+        ResourceException exception  = assertThrows(ResourceException.class, ()-> OasisCppa3CppHandler.validateOasisCPPASchema(OasisCppa3CppHandlerTest.class.getResourceAsStream("/examples/signed-cpp-invalid.xml")));
+        assertEquals(SAXParseException.class, exception.getCause().getClass());
     }
 }
