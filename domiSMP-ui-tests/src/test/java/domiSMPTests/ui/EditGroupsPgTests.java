451f05dd6a1154c3c4db64fe38241fac710459c3
Mihai BOZ <Mihai.BOZ@ext.ec.europa.eu>
2023-09-29 18:27:25 +0300

    added rest call for AddResourceToDomain

--- domiSMP-ui-tests/src/test/java/domiSMPTests/ui/EditGroupsPgTests.java
+++ domiSMP-ui-tests/src/test/java/domiSMPTests/ui/EditGroupsPgTests.java
@@ -2,6 +2,7 @@ package domiSMPTests.ui;
 
 import ddsl.DomiSMPPage;
 import ddsl.enums.Pages;
+import ddsl.enums.ResourceTypes;
 import domiSMPTests.SeleniumTest;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +13,9 @@ import pages.administration.editDomainsPage.EditDomainsPage;
 import rest.models.DomainModel;
 import rest.models.MemberModel;
 import rest.models.UserModel;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EditGroupsPgTests extends SeleniumTest {
     DomiSMPPage homePage;
@@ -36,6 +40,10 @@ public class EditGroupsPgTests extends SeleniumTest {
         JSONObject domainJson = rest.domains().createDomain(domainModel);
         domainId = domainJson.get("domainId").toString();
         rest.domains().addMembersToDomain(domainId, domainMember);
+        List<ResourceTypes> resourcesToBeAdded = Arrays.asList(ResourceTypes.OASIS1, ResourceTypes.OASIS3, ResourceTypes.OASIS2);
+
+
+        rest.domains().addResourcesToDomain(domainId, resourcesToBeAdded);
 
         homePage = new DomiSMPPage(driver);
         loginPage = homePage.goToLoginPage();
