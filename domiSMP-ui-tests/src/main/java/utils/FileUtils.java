f863e60812c4850fc8ce43309615e8f7700acd93
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2023-10-09 09:42:32 +0200

    Add keystore import example with mat-select

--- domiSMP-ui-tests/src/main/java/utils/FileUtils.java
+++ domiSMP-ui-tests/src/main/java/utils/FileUtils.java
@@ -1,10 +1,26 @@
 package utils;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtils {
+
+    private static final Path keystoreFolder = Paths.get("src", "main", "resources", "keystore");
+
+
     public static String getAbsolutePath(String relativePath) {
         return new File(relativePath).getAbsolutePath();
+    }
+
+    /**
+     * Returns the absolute path of the keystore file in the keystore folder
+     *
+     * @param keystoreFileName the name of the keystore file
+     * @return the absolute path of the keystore file
+     */
+    public static String getAbsoluteKeystorePath(String keystoreFileName) {
+        return keystoreFolder.resolve(keystoreFileName).toAbsolutePath().toString();
     }
 
 }
