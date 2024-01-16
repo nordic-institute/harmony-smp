da9c332aa3674dbea3bb7d61eb9e8aa34fb7d915
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2024-01-16 12:27:38 +0100

    Fix build

--- domismp-tests/domismp-docker/compose/domismp-wls122-oradb/stopClearCompose.sh
+++ domismp-tests/domismp-docker/compose/domismp-wls122-oradb/stopClearCompose.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-WORKDIR="$(dirname $0)"
-SMP_PROJECT_FOLDER=$(readlink -e "${WORKDIR}/../../..")
-source "${SMP_PROJECT_FOLDER}/smp-docker/functions/common.functions"
-source "${SMP_PROJECT_FOLDER}/smp-docker/functions/run-test.functions"
-[ -f "${WORKDIR}/.env" ] && source "${WORKDIR}/.env"
-cd "${WORKDIR}" || exit 100
-initializeCommonVariables
-discoverApplicationVersion
+# init plan variables
+WORKDIR="$(cd -P $(dirname ${BASH_SOURCE[0]} ) && pwd)"
+source "${WORKDIR}/../../functions/run-test.functions"
+initializeVariables
 
 echo "Clear old containers"
 stopAndClearTestContainers
