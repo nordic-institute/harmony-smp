5f8bc6736ba35f746b2936b054ac304617aa41a1
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2024-10-23 10:46:36 +0200

    updating the docker scripts to execute the tests

--- domismp-tests/domismp-docker/compose/domismp-springboot-mysql/runTestsAPI.sh
+++ domismp-tests/domismp-docker/compose/domismp-springboot-mysql/runTestsAPI.sh
@@ -5,19 +5,4 @@ WORKDIR="$(cd -P $(dirname ${BASH_SOURCE[0]} ) && pwd)"
 source "${WORKDIR}/../../functions/run-test.functions"
 initializeVariables
 
-RESULT_FOLDER="${WORKDIR}/results"
-SMP_VERSION_SUFFIX=$1
-if [ -n "${SMP_VERSION_SUFFIX}" ]; then
-    SMP_VERSION_SUFFIX="-${SMP_VERSION_SUFFIX}"
-fi
-# clear old results
-rm -rf "${RESULT_FOLDER}"
-mkdir -p "${RESULT_FOLDER}"
-
-# define network to connect the tests
-DOCKER_NETWORK_NAME="${PLAN_PREFIX}_default"
-export DOCKER_NETWORK_NAME
-
-# Starting Docker Compose TEST (in specific project to avoid orphan container warning)
-docker compose -f docker-compose.test-api${SMP_VERSION_SUFFIX}.yml -p "run-${PLAN_PREFIX}" up
-docker cp "run-${PLAN_PREFIX}-testapi-1:/data/results/soapui-reports" "./results${SMP_VERSION_SUFFIX}"
+runTestsFromContainer "testapi" "test-api" "$@"
