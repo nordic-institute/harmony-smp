80577e8672e2b3c6af2d66e99b782932c8c2da37
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2025-02-18 15:22:31 +0100

    EDELIVERY-14521 [UI Document editor] clear selected subresource on resource select

--- smp-angular/src/app/edit/edit-resources/resource-details-panel/resource-details-panel.component.ts
+++ smp-angular/src/app/edit/edit-resources/resource-details-panel/resource-details-panel.component.ts
@@ -108,6 +108,8 @@ export class ResourceDetailsPanelComponent implements BeforeLeaveGuard {
   async onShowButtonDocumentClicked() {
     // set selected resource
     this.editResourceService.selectedResource = this.resource;
+    // clear selected subresource
+    this.editResourceService.selectedSubresource = null;
 
     let node: NavigationNode = await this.createNewDocumentNavigationNode();
     this.navigationService.selected.children = [node]
