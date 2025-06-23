8b838bb07b3024d1acea7ea7dec51daa675143e3
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2025-06-23 13:43:43 +0200

    Fix delete certificate issue

--- smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/config/UISecurityConfig.java
+++ smp-webapp/src/main/java/eu/europa/ec/edelivery/smp/config/UISecurityConfig.java
@@ -178,6 +178,8 @@ public class UISecurityConfig {
                                 SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
                         .requestMatchers(matcherBuilder.matcher(HttpMethod.DELETE, "/ui/internal/rest/**")).hasAnyAuthority(
                                 SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/ui/internal/rest/**")).hasAnyAuthority(
+                                SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
                         .requestMatchers(matcherBuilder.matcher(HttpMethod.POST, "/ui/edit/rest/**")).hasAnyAuthority(
                                 SMPAuthority.S_AUTHORITY_USER.getAuthority(),
                                 SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
@@ -185,6 +187,9 @@ public class UISecurityConfig {
                                 SMPAuthority.S_AUTHORITY_USER.getAuthority(),
                                 SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
                         .requestMatchers(matcherBuilder.matcher(HttpMethod.DELETE, "/ui/edit/rest/**")).hasAnyAuthority(
+                                SMPAuthority.S_AUTHORITY_USER.getAuthority(),
+                                SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/ui/edit/rest/**")).hasAnyAuthority(
                                 SMPAuthority.S_AUTHORITY_USER.getAuthority(),
                                 SMPAuthority.S_AUTHORITY_SYSTEM_ADMIN.getAuthority())
                         .requestMatchers(matcherBuilder.matcher(HttpMethod.PUT, "/ui/internal/rest/**")).hasAnyAuthority(
