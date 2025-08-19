af701ba8ecb35625789da03218a1695a0b9942bb
Sebastian-Ion TINCU <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2025-08-19 14:38:41 +0300

    Edelivery-15447 Translate Server Side Error Messages For Exceptions
    Add support for translating exception messages in the error controller advice.

--- smp-webapp/src/test/java/eu/europa/ec/edelivery/smp/error/ServiceErrorControllerAdviceTest.java
+++ smp-webapp/src/test/java/eu/europa/ec/edelivery/smp/error/ServiceErrorControllerAdviceTest.java
@@ -22,17 +22,37 @@ import eu.europa.ec.dynamicdiscovery.exception.MalformedIdentifierException;
 import eu.europa.ec.edelivery.smp.error.xml.ErrorResponse;
 import eu.europa.ec.edelivery.smp.exceptions.BadRequestException;
 import eu.europa.ec.edelivery.smp.exceptions.ErrorBusinessCode;
+import eu.europa.ec.edelivery.smp.services.ConfigurationService;
+import eu.europa.ec.edelivery.smp.services.SMPExceptionLanguageService;
+import eu.europa.ec.edelivery.smp.services.SMPLanguageResourceService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.*;
 
 class ServiceErrorControllerAdviceTest {
 
-    ServiceErrorControllerAdvice testIntance = new ServiceErrorControllerAdvice();
+    File localeFolder = new File("target/locales");
+    ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+    ConfigurationService configurationService = Mockito.mock(ConfigurationService.class);
+    SMPLanguageResourceService smpLanguageResourceService = new SMPLanguageResourceService(configurationService, resourcePatternResolver);
+    SMPExceptionLanguageService smpExceptionLanguageService = new SMPExceptionLanguageService(smpLanguageResourceService);
+
+    ServiceErrorControllerAdvice testIntance = new ServiceErrorControllerAdvice(smpExceptionLanguageService);
+
+    @BeforeEach
+    public void setup() {
+        Mockito.when(configurationService.getLocaleFolder()).thenReturn(localeFolder);
+    }
 
     @Test
     void handleRuntimeException() {
@@ -40,8 +60,6 @@ class ServiceErrorControllerAdviceTest {
 
         assertEquals(INTERNAL_SERVER_ERROR, re.getStatusCode());
         assertEquals(ErrorBusinessCode.TECHNICAL.toString(), ((ErrorResponse)re.getBody()).getBusinessCode());
-
-
     }
 
     @Test
